80320d7fd0d2e1ccbd047f22dcef98c2a146b06f
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-27 19:58:36 +0900

    Title: 1차원 폭발 게임, Time: 1084ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240127/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 240127/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -6,32 +6,32 @@ int N, M;
 vector<int> bombs;
 
 void Simulate() {
-	int cnt = 1;
-	vector<pair<int, int>> explode_pair;
-	for (int i = 0; i + 1 < bombs.size(); i++) {
-		if (bombs[i] == bombs[i + 1]) cnt++;
-		else{
-			if (cnt >= M) explode_pair.push_back({ i +1 - cnt, i });
-			cnt = 1;
+	while (true) {
+		int cnt = 1;
+		vector<pair<int, int>> explode_pair;
+		for (int i = 0; i + 1 < bombs.size(); i++) {
+			if (bombs[i] == bombs[i + 1]) cnt++;
+			else {
+				if (cnt >= M) explode_pair.push_back({ i + 1 - cnt, i });
+				cnt = 1;
+			}
 		}
-	}
-	if (cnt >= M) explode_pair.push_back({ bombs.size()- 1 - cnt+1, bombs.size() - 1});
-	
-	if (explode_pair.size() == 0) return;
+		if (cnt >= M) explode_pair.push_back({ bombs.size() - 1 - cnt + 1, bombs.size() - 1 });
 
-	for (int i = 0; i < explode_pair.size(); i++) {
-		for (int j = explode_pair[i].first; j <= explode_pair[i].second;j++) {
-			bombs[j] = 0;
+		if (explode_pair.size() == 0) return;
+
+		for (int i = 0; i < explode_pair.size(); i++) {
+			for (int j = explode_pair[i].first; j <= explode_pair[i].second; j++) {
+				bombs[j] = 0;
+			}
 		}
-	}
 
-	vector<int> tmp;
-	for (int i = 0; i < bombs.size(); i++) {
-		if (bombs[i] != 0) tmp.push_back(bombs[i]);
+		vector<int> tmp;
+		for (int i = 0; i < bombs.size(); i++) {
+			if (bombs[i] != 0) tmp.push_back(bombs[i]);
+		}
+		bombs = tmp;
 	}
-	bombs = tmp;
-
-	Simulate();
 }
 
 int main() {
