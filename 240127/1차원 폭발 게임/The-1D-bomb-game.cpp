d270e19a4cd660246172c6cff60b3c8a6b753470
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-27 20:37:20 +0900

    Title: 1차원 폭발 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240127/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 240127/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -6,53 +6,81 @@ int N, M;
 vector<int> bombs;
 
 void Simulate() {
-    while (true) {
-        vector<int> tmp; // 임시 벡터
-        bool exploded = false; // 폭발 발생 여부
-        int cnt = 1;
+	
+	while (true) {
+		int cnt = 1;
+		vector<pair<int, int>> explode_pair;
 
-        for (int i = 0; i < bombs.size(); i++) {
-            // 연속된 같은 숫자 세기
-            if (i < bombs.size() - 1 && bombs[i] == bombs[i + 1]) {
-                cnt++;
-            } else {
-                // 폭발 기준에 도달하면 폭발 처리
-                if (cnt >= M) {
-                    exploded = true;
-                    cnt = 1;
-                    continue;
-                }
+		for (int i = 0; i < bombs.size(); i++) {
+			if (i + 1 < bombs.size() && bombs[i] == bombs[i + 1]) cnt++;
+			else {
+				if (cnt >= M) explode_pair.push_back({ i + 1 - cnt, i });
+				cnt = 1;
+			}
+		}
 
-                // 폭발하지 않은 경우 벡터에 추가
-                while (cnt > 0) {
-                    tmp.push_back(bombs[i - cnt + 1]);
-                    cnt--;
-                }
-                cnt = 1;
-            }
-        }
+		if (explode_pair.size() == 0) return;
 
-        // 폭발이 발생하지 않았으면 종료
-        if (!exploded) break;
+		for (int i = 0; i < explode_pair.size(); i++) {
+			for (int j = explode_pair[i].first; j <= explode_pair[i].second; j++) {
+				bombs[j] = 0;
+			}
+		}
 
-        // bombs 벡터 업데이트
-        bombs = tmp;
-    }
+		vector<int> tmp;
+		for (int i = 0; i < bombs.size(); i++) {
+			if (bombs[i] != 0) tmp.push_back(bombs[i]);
+		}
+
+		bombs = tmp;
+	}
+	
+
+	/*
+	while (true) {
+		vector<int> tmp;
+		bool exploded = false;
+		int cnt = 1;
+
+		for (int i = 0; i < bombs.size(); i++) {
+			if (i < bombs.size() - 1 && bombs[i] == bombs[i + 1]) {
+				cnt++;
+			}
+			else {
+				if (cnt >= M) {
+					exploded = true;
+					cnt = 1;
+					continue;
+				}
+
+				while (cnt > 0) {
+					tmp.push_back(bombs[i - cnt + 1]);
+					cnt--;
+				}
+				cnt = 1;
+			}
+		}
+
+		if (!exploded) break;
+
+		bombs = tmp;
+	}
+	*/
 }
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> N >> M;
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> M;
+	
+	bombs.resize(N);
+	for (int i = 0; i < N; i++) {
+		cin >> bombs[i];
+	}
 
-    bombs.resize(N);
-    for (int i = 0; i < N; i++) {
-        cin >> bombs[i];
-    }
+	Simulate();
 
-    Simulate();
-
-    cout << bombs.size() << "\n";
-    for (int i = 0; i < bombs.size(); i++) {
-        cout << bombs[i] << "\n";
-    }
+	cout << bombs.size() << "\n";
+	for (int i = 0; i < bombs.size(); i++) {
+		cout << bombs[i] << "\n";
+	}
 }
