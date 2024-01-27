911e1aedf1931112c9cb060c6b8cad7d4a0512dd
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-27 19:43:31 +0900

    Title: 1차원 폭발 게임, Time: 243ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240127/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+
+void Simulate(vector<int>& bombs) {
+	int cnt = 1;
+	vector<pair<int, int>> explode_pair;
+	for (int i = 0; i + 1 < bombs.size(); i++) {
+		if (bombs[i] == bombs[i + 1]) cnt++;
+		else{
+			if (cnt >= M) explode_pair.push_back({ i +1 - cnt, i });
+			cnt = 1;
+		}
+	}
+	if (cnt >= M) explode_pair.push_back({ bombs.size()- 1 - cnt+1, bombs.size() - 1});
+	
+	if (explode_pair.size() == 0) return;
+
+	for (int i = 0; i < explode_pair.size(); i++) {
+		for (int j = explode_pair[i].first; j <= explode_pair[i].second;j++) {
+			bombs[j] = 0;
+		}
+	}
+
+	vector<int> tmp;
+	for (int i = 0; i < bombs.size(); i++) {
+		if (bombs[i] != 0) tmp.push_back(bombs[i]);
+	}
+	bombs = tmp;
+
+	Simulate(bombs);
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> M;
+	vector<int> bombs;
+	for (int i = 0; i < N; i++) {
+		int x; cin >> x;
+		bombs.push_back(x);
+	}
+
+	Simulate(bombs);
+
+	cout << bombs.size() << "\n";
+	for (int i = 0; i < bombs.size(); i++) {
+		cout << bombs[i] << "\n";
+	}
+}
