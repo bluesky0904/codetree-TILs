ced2b3954c0ebcc2a7907ba5cdee53a2c5b831ec
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-28 22:11:15 +0900

    Title: 단 한 번의 2048 시도, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240128/단 한 번의 2048 시도/one-trial-of-2048-game.cpp
+++ 240128/단 한 번의 2048 시도/one-trial-of-2048-game.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-#define MAX_N 4
-#define ASCII_NUM 128
 #define NONE -1
+#define ASCII_NUM 128
 
 int n = 4;
 
-vector<vector<int>> grid(MAX_N, vector<int>(MAX_N));
-vector<vector<int>> next_grid(MAX_N, vector<int>(MAX_N));
-
+vector<vector<int>> grid(n, vector<int>(n));
+vector<vector<int>> next_grid(n, vector<int>(n));
 
 void Rotate() {
 	for (int i = 0; i < n; i++)
@@ -20,7 +17,7 @@ void Rotate() {
 
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			next_grid[i][j] = grid[n - j - 1][i];
+			next_grid[i][j] = grid[n-j-1][i];
 
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
@@ -33,21 +30,22 @@ void Drop() {
 			next_grid[i][j] = 0;
 
 	for (int j = 0; j < n; j++) {
-		int keep_num = NONE, next_row = n - 1;
+		int keep_num = NONE;
+		int idx = n - 1;
 		for (int i = n - 1; i >= 0; i--) {
 			if (!grid[i][j]) continue;
 
 			if (keep_num == NONE) keep_num = grid[i][j];
 			else if (keep_num == grid[i][j]) {
-				next_grid[next_row--][j] = keep_num * 2;
+				next_grid[idx--][j] = 2 * keep_num;
 				keep_num = NONE;
 			}
 			else {
-				next_grid[next_row--][j] = keep_num;
+				next_grid[idx--][j] = keep_num;
 				keep_num = grid[i][j];
 			}
 		}
-		if (keep_num != NONE) next_grid[next_row--][j] = keep_num;
+		if (keep_num != NONE) next_grid[idx--][j] = keep_num;
 	}
 
 	for (int i = 0; i < n; i++)
@@ -55,32 +53,25 @@ void Drop() {
 			grid[i][j] = next_grid[i][j];
 }
 
-void Tilt(int move_dir) {
-	for (int i = 0; i < move_dir; i++) Rotate();
-
+void Tilt(int dir_num) {
+	for (int i = 0; i < dir_num; i++) Rotate();
 	Drop();
-
-	for (int i = 0; i < 4 - move_dir; i++) Rotate();
+	for (int i = 0; i < 4- dir_num; i++) Rotate();
 }
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
 			cin >> grid[i][j];
-		}
-	}
 
-	char dir;
-	cin >> dir;
-
-	int dir_mapper[ASCII_NUM];
-	dir_mapper['D'] = 0;
-	dir_mapper['R'] = 1;
-	dir_mapper['U'] = 2;
-	dir_mapper['L'] = 3;
-
-	Tilt(dir_mapper[dir]);
+	char dir; cin >> dir;
+	int dir_num[ASCII_NUM];
+	dir_num['D'] = 0;
+	dir_num['R'] = 1;
+	dir_num['U'] = 2;
+	dir_num['L'] = 3;
+	Tilt(dir_num[dir]);
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
@@ -88,6 +79,6 @@ int main() {
 		}
 		cout << "\n";
 	}
-
+	
 	return 0;
 }
