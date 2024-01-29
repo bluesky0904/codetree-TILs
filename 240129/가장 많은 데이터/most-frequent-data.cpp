8bd4501a9911d23025889fc54768e3d3b326aa05
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-29 21:11:38 +0900

    Title: 가장 많은 데이터, Time: 180ms, Memory: 19MB, Status: Passed - CodeTree

--- 240129/가장 많은 데이터/most-frequent-data.cpp
+++ 240129/가장 많은 데이터/most-frequent-data.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
-#include <string>
 #include <unordered_map>
-using namespace std;
+#include <string>
+#include <algorithm>
 
 #define MAX_N 100000
 
+using namespace std;
+
+// 변수 선언
 int n;
-string arr[MAX_N];
+string words[MAX_N];
 unordered_map<string, int> freq;
 
-int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-		freq[arr[i]]++;
-	}
+int ans; // 가장 많이 나온 횟수를 기록합니다.
 
-	int ans = 0;
-	for (auto it = freq.begin(); it != freq.end(); it++) {
-		if (it->second > ans) ans = it->second;
-	}
-	cout << ans << " ";
+int main() {
+    // 입력:
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> words[i];
+    
+    // 각 문자열이 몇 번씩 나왔는지를
+    // hashmap에 기록해줍니다.
+    for(int i = 0; i < n; i++) {
+        // 처음 나온 문자열이라면 1을 직접 적어줘야 합니다.
+        if(freq.find(words[i]) == freq.end())
+            freq[words[i]] = 1;
+        // 이미 나와있던 문자열이라면 1을 더해줍니다.
+        else
+            freq[words[i]]++;
+        
+        // 최댓값을 갱신합니다.
+        ans = max(ans, freq[words[i]]);
+    }
+
+    cout << ans;
+    return 0;
 }
