8801bb5d455e4490b3cb7cd7f96fb9dd609aeb8c
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-29 22:53:54 +0900

    Title: 대응되는 수와 문자, Time: 340ms, Memory: 12MB, Status: Passed - CodeTree

--- 240129/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
+++ 240129/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -1,40 +1,28 @@
 #include <iostream>
+#include <string>
 #include <unordered_map>
-#include <cctype>
+
 using namespace std;
 
 #define MAX_N 100000
 
 int n, m;
-unordered_map<string, int> string_to_index;
-unordered_map<int, string> index_to_string;
-
-bool isNumber(const string& str) {
-    for (char const &c : str) {
-        if (!isdigit(c)) return false;
-    }
-    return true;
-}
+string words[MAX_N + 1];
+unordered_map<string, int> string_to_num;
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        string temp;
-        cin >> temp;
-        string_to_index[temp] = i + 1;
-        index_to_string[i + 1] = temp;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> words[i];
+        string_to_num[words[i]] = i;
     }
 
-    for (int i = 0; i < m; i++) {
-        string command; cin >> command;
-        if (isNumber(command)) {
-            int num = stoi(command);
-            cout << index_to_string[num] << "\n";
-        }
-        else {
-            cout << string_to_index[command] << "\n";
-        }
+    while (m--) {
+        string key; cin >> key;
+
+        if ('0' <= key[0] && key[0] <= '9') cout << words[stoi(key)] << "\n";
+        else cout << string_to_num[key] << "\n";
     }
     return 0;
 }
