5956efc5e9aaaf4dfea66441e71530763cb66a55
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-29 22:47:59 +0900

    Title: 대응되는 수와 문자, Time: 144ms, Memory: 16MB, Status: Passed - CodeTree

--- 240129/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
+++ 240129/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -1,46 +1,40 @@
 #include <iostream>
 #include <unordered_map>
-#include <sstream>
+#include <cctype>
 using namespace std;
 
 #define MAX_N 100000
 
 int n, m;
-string arr[MAX_N];
 unordered_map<string, int> string_to_index;
+unordered_map<int, string> index_to_string;
 
 bool isNumber(const string& str) {
-	stringstream ss(str);
-	double d;
-	char c;
-	if (!(ss >> d)) return false;
-	if (ss >> c) return false;
-	return true;
+    for (char const &c : str) {
+        if (!isdigit(c)) return false;
+    }
+    return true;
 }
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-		string_to_index[arr[i]] = i + 1;
-	}
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        string temp;
+        cin >> temp;
+        string_to_index[temp] = i + 1;
+        index_to_string[i + 1] = temp;
+    }
 
-	for (int i = 0; i < m; i++) {
-		string command; cin >> command;
-		if (isNumber(command)) {
-			// 숫자일 경우 key값인 문자열을 출력해야 하는데 어떻게 하지?
-			int num = stoi(command);
-			for (auto it = string_to_index.begin(); it != string_to_index.end(); it++) {
-				if (it->second == num) {
-					cout << it->first << "\n";
-					break;
-				}
-			}
-		}
-		else {
-			cout << string_to_index[command] << "\n";
-		}
-	}
-	return 0;
+    for (int i = 0; i < m; i++) {
+        string command; cin >> command;
+        if (isNumber(command)) {
+            int num = stoi(command);
+            cout << index_to_string[num] << "\n";
+        }
+        else {
+            cout << string_to_index[command] << "\n";
+        }
+    }
+    return 0;
 }
