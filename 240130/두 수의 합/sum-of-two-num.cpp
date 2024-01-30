958b5ad15a6be769ef84609128ed8cc19057c3af
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-30 21:30:04 +0900

    Title: 두 수의 합, Time: 66ms, Memory: 6MB, Status: Passed - CodeTree

--- 240130/두 수의 합/sum-of-two-num.cpp
+++ 240130/두 수의 합/sum-of-two-num.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <unordered_map>
-
 using namespace std;
 
 #define MAX_N 100000
-
 int n, k;
 int arr[MAX_N];
 unordered_map<long long, int> cnt;
 
 int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	cin >> n >> k;
 	for (int i = 0; i < n; i++) cin >> arr[i];
 	int ans = 0;
-
 	for (int i = 0; i < n; i++) {
 		long long diff = (long long)k - arr[i];
 		ans += cnt[diff];
 		cnt[arr[i]]++;
 	}
-	cout << ans;
+	cout << ans << "\n";
 	return 0;
 }
