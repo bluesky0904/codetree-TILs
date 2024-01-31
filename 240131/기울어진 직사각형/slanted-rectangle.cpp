df2738411858c25a55f579695adca4a5ffb639d0
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-31 14:58:25 +0900

    Title: 기울어진 직사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240131/기울어진 직사각형/slanted-rectangle.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_N 20
+#define DIR_NUM 4
+
+int n;
+int grid[MAX_N][MAX_N];
+int dx[DIR_NUM] = {-1,-1, 1, 1};
+int dy[DIR_NUM] = {1,-1, -1, 1};
+
+bool InRange(int x, int y) {
+	return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+int RecSum(int x, int y, int width, int length) {
+	int cx = x, cy = y;
+	int rec_sum = grid[cx][cy];
+	int nx, ny;
+	for (int i = 1; i < width; i++) {
+		nx = cx + dx[0];
+		ny = cy + dy[0];
+		if (InRange(nx, ny)) {
+			rec_sum += grid[nx][ny];
+			cx = nx;
+			cy = ny;
+		}
+		else return -1;
+	}
+	for (int i = 1; i < length; i++) {
+		nx = cx + dx[1];
+		ny = cy + dy[1];
+		if (InRange(nx, ny)) {
+			rec_sum += grid[nx][ny];
+			cx = nx;
+			cy = ny;
+		}
+		else return -1;
+	}
+	for (int i = 1; i < width; i++) {
+		nx = cx + dx[2];
+		ny = cy + dy[2];
+		if (InRange(nx, ny)) {
+			rec_sum += grid[nx][ny];
+			cx = nx;
+			cy = ny;
+		}
+		else return -1;
+	}
+	for (int i = 1; i < length-1; i++) {
+		nx = cx + dx[3];
+		ny = cy + dy[3];
+		if (InRange(nx, ny)) {
+			rec_sum += grid[nx][ny];
+			cx = nx;
+			cy = ny;
+		}
+		else return -1;
+	}
+	return rec_sum;
+}
+
+int Simulate(int x, int y) {
+	int max_val = -1;
+	for (int width = 1; width < n; width++) {
+		for (int length = 1; length < n; length++) {
+			max_val = max(max_val, RecSum(x, y, width, length));
+		}
+	}
+	return max_val;
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> grid[i][j];
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			ans = max(ans, Simulate(i, j));
+		}
+	}
+
+	cout << ans << "\n";
+}
