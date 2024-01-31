5984f11017b3b9b20470668931c7a5f59210ec44
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-31 20:22:53 +0900

    Title: 자주 등장한 top k 숫자, Time: 42ms, Memory: 4MB, Status: Passed - CodeTree

--- 240131/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
+++ 240131/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
@@ -9,26 +9,23 @@ using namespace std;
 int n, k;
 int arr[MAX_N];
 unordered_map<int, int> freq;
+vector<pair<int, int>> v;
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    cin >> n >> k;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        freq[arr[i]]++;
-    }
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		freq[arr[i]]++;
+	}
 
-    vector<pair<int, int>> freq_vec;
-    for (auto& p : freq) {
-        freq_vec.push_back(make_pair(p.second, p.first));
-    }
+	for (auto it = freq.begin(); it != freq.end(); it++) {
+		v.push_back({it->second, it->first});
+	}
 
-    sort(freq_vec.rbegin(), freq_vec.rend()); // 빈도수가 높은 순, 그리고 값이 큰 순으로 정렬
+	sort(v.begin(), v.end());
 
-    for (int i = 0; i < k; i++) {
-        cout << freq_vec[i].second << " "; // 가장 자주 등장하는 숫자부터 출력
-    }
-    cout << "\n";
-
-    return 0;
+	for (int i = v.size() - 1; k > 0; i--, k--) {
+		cout << v[i].second << " ";
+	}
 }
