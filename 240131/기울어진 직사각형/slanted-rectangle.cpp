43edf787425f119762b8bbb8e85d4371f78625da
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-01-31 15:02:34 +0900

    Title: 기울어진 직사각형, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240131/기울어진 직사각형/slanted-rectangle.cpp
+++ 240131/기울어진 직사각형/slanted-rectangle.cpp
@@ -1,91 +1,61 @@
 #include <iostream>
 #include <algorithm>
-using namespace std;
 
 #define MAX_N 20
 #define DIR_NUM 4
 
+using namespace std;
+
 int n;
 int grid[MAX_N][MAX_N];
-int dx[DIR_NUM] = {-1,-1, 1, 1};
-int dy[DIR_NUM] = {1,-1, -1, 1};
 
 bool InRange(int x, int y) {
-	return 0 <= x && x < n && 0 <= y && y < n;
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-int RecSum(int x, int y, int width, int length) {
-	int cx = x, cy = y;
-	int rec_sum = 0;
-	int nx, ny;
-	for (int i = 1; i < width; i++) {
-		nx = cx + dx[0];
-		ny = cy + dy[0];
-		if (InRange(nx, ny)) {
-			rec_sum += grid[nx][ny];
-			cx = nx;
-			cy = ny;
-		}
-		else return -1;
-	}
-	for (int i = 1; i < length; i++) {
-		nx = cx + dx[1];
-		ny = cy + dy[1];
-		if (InRange(nx, ny)) {
-			rec_sum += grid[nx][ny];
-			cx = nx;
-			cy = ny;
-		}
-		else return -1;
-	}
-	for (int i = 1; i < width; i++) {
-		nx = cx + dx[2];
-		ny = cy + dy[2];
-		if (InRange(nx, ny)) {
-			rec_sum += grid[nx][ny];
-			cx = nx;
-			cy = ny;
-		}
-		else return -1;
-	}
-	for (int i = 1; i < length; i++) {
-		nx = cx + dx[3];
-		ny = cy + dy[3];
-		if (InRange(nx, ny)) {
-			rec_sum += grid[nx][ny];
-			cx = nx;
-			cy = ny;
-		}
-		else return -1;
-	}
-	return rec_sum;
-}
+int GetScore(int x, int y, int k, int l) {
+    int dx[DIR_NUM] = {-1, -1, 1, 1};
+    int dy[DIR_NUM] = {1, -1, -1, 1};
+    int move_num[DIR_NUM] = {k, l, k, l};
+    
+    int sum_of_nums = 0;
 
-int Simulate(int x, int y) {
-	int max_val = -1;
-	for (int width = 1; width < n; width++) {
-		for (int length = 1; length < n; length++) {
-			max_val = max(max_val, RecSum(x, y, width, length));
-		}
-	}
-	return max_val;
+    // 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+    for(int d = 0; d < DIR_NUM; d++)
+        for(int q = 0; q < move_num[d]; q++) {
+            x += dx[d]; y += dy[d];
+                
+            // 기울어진 직사각형이 경계를 벗어나는 경우라면
+            // 불가능하다는 의미로 답이 갱신되지 않도록
+            // 0을 반환합니다.
+            if(!InRange(x, y))
+                return 0;
+			
+            sum_of_nums += grid[x][y];
+        }
+    
+    return sum_of_nums;
 }
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> grid[i][j];
-		}
-	}
+    cin >> n;
+    
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> grid[i][j];
+    
+    int ans = 0;
+    
+    // (i, j)를 시작으로 1, 2, 3, 4 방향
+    // 순서대로 길이 [k, l, k, l] 만큼 이동하면 그려지는
+    // 기울어진 직사각형을 잡아보는
+    // 완전탐색을 진행해봅니다.
+    for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			for(int k = 1; k < n; k++)
+				for(int l = 1; l < n; l++)
+                    ans = max(ans, GetScore(i, j, k, l));
 
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			ans = max(ans, Simulate(i, j));
-		}
-	}
-
-	cout << ans << "\n";
+    cout << ans;
+    return 0;
 }
