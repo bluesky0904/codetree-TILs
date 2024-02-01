29f76eb4ef33f533c4e0f15d3f2f5664b9746afb
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-01 20:15:41 +0900

    Title: 양수 직사각형의 최대 크기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240201/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_N 20
+int n, m;
+int grid[MAX_N][MAX_N];
+
+bool IsPositiveRec(int sx, int sy, int ex, int ey) {
+	for (int i = sx; i <= ex; i++) {
+		for (int j = sy; j <= ey; j++) {
+			if (grid[i][j] < 0) return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> grid[i][j];
+		}
+	}
+
+	int ans = -1;
+	for (int sx = 0; sx < n; sx++) {
+		for (int sy = 0; sy < m; sy++) {
+			for (int ex = 0; ex < n; ex++) {
+				for (int ey = 0; ey < m; ey++) {
+					if (IsPositiveRec(sx, sy, ex, ey)) {
+						ans = max(ans, (ex - sx + 1) * (ey - sy + 1));
+					}
+				}
+			}
+		}
+	}
+	cout << ans << "\n";
+
+	return 0;
+}
