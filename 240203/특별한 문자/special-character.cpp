147cea30bfc500192882dabe553ce740ecce9a72
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-03 21:37:23 +0900

    Title: 특별한 문자, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/특별한 문자/special-character.cpp
+++ 240203/특별한 문자/special-character.cpp
@@ -6,18 +6,18 @@ using namespace std;
 unordered_map<char, int> freq;
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	string s; cin >> s;
-	for (int i = 0; i < s.size(); i++) {
-		freq[s[i]]++;
-	}
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    string s; cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        freq[s[i]]++;
+    }
 
-	for (auto it = freq.begin(); it != freq.end(); it++) {
-		if (it->second == 1) {
-			cout << it->first << "\n";
-			return 0;
-		}
-	}
-	cout << "None" << "\n";
-	return 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (freq[s[i]] == 1) {
+            cout << s[i] << "\n";
+            return 0;
+        }
+    }
+    cout << "None" << "\n";
+    return 0;
 }
