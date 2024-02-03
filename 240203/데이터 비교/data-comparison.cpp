dd4c639c9b63ccaf99ca723775ad68eaad90c0a7
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-03 23:46:24 +0900

    Title: 데이터 비교, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

--- 240203/데이터 비교/data-comparison.cpp
+++ 240203/데이터 비교/data-comparison.cpp
@@ -3,20 +3,26 @@
 using namespace std;
 
 #define MAX_NUM 100000
+
 int n, m;
 int arr1[MAX_NUM];
 int arr2[MAX_NUM];
+unordered_set<int> set1;
+unordered_set<int> set2;
 
 int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	cin >> n;
-	for (int i = 0; i < n; i++) cin >> arr1[i];
-	unordered_set<int> set1;
-	for (int i = 0; i < n; i++) set1.insert(arr1[i]);
+	for (int i = 0; i < n; i++) {
+		cin >> arr1[i];
+		set1.insert(arr1[i]);
+	}
 
 	cin >> m;
-	for (int i = 0; i < m; i++) cin >> arr2[i];
-	unordered_set<int> set2;
-	for (int i = 0; i < m; i++) set2.insert(arr2[i]);
+	for (int i = 0; i < m; i++) {
+		cin >> arr2[i];
+		set2.insert(arr2[i]);
+	}
 
 	for (int i = 0; i < m; i++) {
 		if (set1.find(arr2[i]) == set1.end()) cout << 0 << " ";
