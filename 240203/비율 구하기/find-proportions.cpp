b32be1834aaec16fabecf5642b20ac06affbbca6
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-03 22:51:46 +0900

    Title: 비율 구하기, Time: 19ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/비율 구하기/find-proportions.cpp
+++ 240203/비율 구하기/find-proportions.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip> // std::fixed와 std::setprecision을 사용하기 위해 필요
 #include <map>
 using namespace std;
 
@@ -7,20 +6,16 @@ int n;
 map<string, int> m;
 
 int main() {
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        string s; cin >> s;
-        m[s]++;
-    }
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string s; cin >> s;
+		m[s]++;
+	}
 
-    int total = 0;
-    for (auto it = m.begin(); it != m.end(); it++) {
-        total += it->second;
-    }
-
-    for (auto it = m.begin(); it != m.end(); it++) {
-        // 부동 소수점 나눗셈을 사용하고, 결과를 소수점 4자리까지 반올림
-        cout << fixed << setprecision(4);
-        cout << it->first << " " << (double)(it->second) * 100 / total << "\n";
-    }
+	for (auto it = m.begin(); it != m.end(); it++) {
+		double ratio = (double)(it->second) / n * 100;
+		cout << fixed;
+		cout.precision(4);
+		cout << (it->first) << " " << ratio << "\n";
+	}
 }
