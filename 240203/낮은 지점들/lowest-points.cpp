8a363b5c451941a9d49f71ea4d400b9997906570
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-03 22:07:35 +0900

    Title: 낮은 지점들, Time: 48ms, Memory: 2MB, Status: Passed - CodeTree

--- 240203/낮은 지점들/lowest-points.cpp
+++ 240203/낮은 지점들/lowest-points.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int n;
-unordered_map<int, int> Coord;
+unordered_map<long long, long long> Coord;
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		int x, y; cin >> x >> y;
+		long long x, y; cin >> x >> y;
 		if (Coord.find(x) == Coord.end() || Coord[x] > y) Coord[x] = y;
 	}
 	
-	int ans = 0;
+	long long ans = 0;
 	for (auto it = Coord.begin(); it != Coord.end(); it++) {
 		ans += it->second;
 	}
