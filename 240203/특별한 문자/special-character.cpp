47fcc10c609baee84906fa4f90cc3179a62d7813
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-03 21:44:38 +0900

    Title: 특별한 문자, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/특별한 문자/special-character.cpp
+++ 240203/특별한 문자/special-character.cpp
@@ -3,21 +3,34 @@
 #include <unordered_map>
 using namespace std;
 
+// 변수 선언
 unordered_map<char, int> freq;
+string str;
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    string s; cin >> s;
-    for (int i = 0; i < s.size(); i++) {
-        freq[s[i]]++;
+    // 입력:
+    cin >> str;
+
+    // 각 알파벳이 몇 번씩 나왔는지를
+    // hashmap에 기록해줍니다.
+    for(int i = 0; i < str.length(); i++) {
+        freq[str[i]]++;
     }
 
-    for (int i = 0; i < s.size(); i++) {
-        if (freq[s[i]] == 1) {
-            cout << s[i] << "\n";
+    // 문자열을 앞에서부터 순회하여
+    // 딱 1회만 등장한 알파벳을 찾습니다.
+    for(int i = 0; i < str.length(); i++) {
+        if(freq[str[i]] == 1) {
+            // 만약 그런 알파벳이 존재한다면,
+            // 알파벳을 출력하고 프로그램을 종료합니다.
+            cout << str[i];
             return 0;
         }
     }
-    cout << "None" << "\n";
+
+    // 문자열을 전부 순회할 때까지 프로그램이 종료되지
+    // 않았으므로 그러한 문자가 없는 경우입니다.
+    cout << "None";
+
     return 0;
 }
