b953d14c4225e6f2f5d65f576aa65b0839f1c4e7
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-03 22:57:38 +0900

    Title: 비율 구하기, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/비율 구하기/find-proportions.cpp
+++ 240203/비율 구하기/find-proportions.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <map>
+#include <string>
 using namespace std;
 
 int n;
 map<string, int> m;
 
 int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		string s; cin >> s;
