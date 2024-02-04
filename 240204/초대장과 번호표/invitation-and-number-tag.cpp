060dec3fbcca5f4fc712af7d24518659a19130d7
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-04 16:46:07 +0900

    Title: 초대장과 번호표, Time: 255ms, Memory: 28MB, Status: Passed - CodeTree

--- 240204/초대장과 번호표/invitation-and-number-tag.cpp
+++ 240204/초대장과 번호표/invitation-and-number-tag.cpp
@@ -1,37 +1,63 @@
 #include <iostream>
-#include <vector>
 #include <unordered_set>
+#include <queue>
+#include <vector>
+
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	int n, g; cin >> n >> g;
-	vector<unordered_set<int>> groups(g);
-	for (int i = 0; i < g; i++) {
-		int group_size; cin >> group_size;
-		for (int j = 0; j < group_size; j++) {
-			int num; cin >> num;
-			groups[i].insert(num);
-		}
-	}
+#define MAX_N 100000
+#define MAX_G 250000
 
-	bool updated = true;
-	unordered_set<int> invited{ 1 };
-	while (updated) {
-		updated = false;
-		for (auto& group : groups) {
-			int cnt = 0;
-			int lastperson;
-			for (int num : group) {
-				if (invited.find(num) == invited.end()) {
-					cnt++;
-					lastperson = num;
-				}
-			}
-			if (cnt == 1) {
-				if(invited.insert(lastperson).second) updated = true;
-			}
-		}
-	}
-	cout << invited.size() << "\n";
+// 변수 선언
+int n, g;
+bool invited[MAX_N];
+// 각 그룹마다 초대장을 받지 못한 사람들을 관리해줍니다.
+unordered_set<int> groups[MAX_G];
+// 각 사람이 어떤 그룹에 속하는지를 관리해줍니다.
+vector<int> people_groups[MAX_N];
+queue<int> q;
+int ans;
+
+int main() {
+    // 입력:
+    cin >> n >> g;
+
+    for(int i = 0; i < g; i++) {
+        int s, x;
+        cin >> s;
+        for(int j = 0; j < s; j++) {
+            cin >> x; x--;
+            groups[i].insert(x);
+            people_groups[x].push_back(i);
+        }
+    }
+
+    q.push(0);
+    invited[0] = true;
+    while(!q.empty()) {
+        int x = q.front();
+        q.pop();
+        ans++;
+
+        // x가 들어있는 그룹에서 x를 지웁니다.
+        // hashset에는 그룹에서 초대받지 않은 인원만을 남깁니다.
+        for(int i = 0; i < (int) people_groups[x].size(); i++) {
+            int g_num = people_groups[x][i];
+
+            // 해당 그룹에서 x를 지웁니다.
+            groups[g_num].erase(x);
+            // 초대받지 않은 인원이 한명밖에 없다면 초대합니다.
+            if((int) groups[g_num].size() == 1) {
+                int p_num = *(groups[g_num].begin());
+                if(!invited[p_num]) {
+                    invited[p_num] = true;
+                    q.push(p_num);
+                }
+            }
+        }
+    }
+
+    // 초대장을 받는 인원을 출력합니다.
+    cout << ans;
+    return 0;
 }
