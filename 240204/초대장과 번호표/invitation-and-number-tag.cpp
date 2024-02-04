5d1b09fb17156a9adf91423cb2f1db3ab82d7ffc
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-04 14:27:47 +0900

    Title: 초대장과 번호표, Time: 9ms, Memory: 5MB, Status: Wrong Answer - CodeTree

---
+++ 240204/초대장과 번호표/invitation-and-number-tag.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+#define MAX_G 250000
+int n, g;
+vector<int> group[MAX_G];
+unordered_set<int> get_ticket;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n >> g;
+	get_ticket.insert(1);
+	for (int i = 0; i < g; i++) {
+		int group_size; cin >> group_size;
+		for (int j = 0; j < group_size; j++) {
+			int num; cin >> num;
+			group[i].push_back(num);
+		}
+	}
+
+	for (int i = 0; i < g; i++) {
+		int cnt = 0;
+		for (int j = 0; j < group[i].size(); j++) {
+			if (get_ticket.find(group[i][j]) != get_ticket.end()) cnt++;
+		}
+		if (cnt + 1 == group[i].size()) {
+			for (int j = 0; j < group[i].size(); j++) {
+				if (get_ticket.find(group[i][j]) == get_ticket.end()) get_ticket.insert(group[i][j]);
+			}
+		}
+	}
+
+	cout << get_ticket.size() << "\n";
+}
