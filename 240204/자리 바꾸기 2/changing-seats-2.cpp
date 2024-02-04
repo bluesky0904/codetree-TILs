0cda33f6edfdf3b2af0b861ee5879d7cf65d0298
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-04 11:10:37 +0900

    Title: 자리 바꾸기 2, Time: 511ms, Memory: 35MB, Status: Passed - CodeTree

--- 240204/자리 바꾸기 2/changing-seats-2.cpp
+++ 240204/자리 바꾸기 2/changing-seats-2.cpp
@@ -1,55 +1,53 @@
-/*
-1~N 번호표, 1~N 자리
-i분에 ai번째 자리의 사람과 bi번째 자리의 사람이 자리 교환
-K분 동안 K번 자리 바꾸기 진행 후 똑같은 방식으로 K번 자리 바꿈이 반복
-3K번에 걸쳐 자리바꿈이 진행될 동안, 각자 몇 군데의 자리에 앉을 수 있는지
-*/
-
 #include <iostream>
 #include <unordered_set>
-#include <vector>
 using namespace std;
 
 #define MAX_N 100000
+#define MAX_K 100000
+
+// 변수 선언
 int n, k;
+int a[MAX_K], b[MAX_K];
+int arr[MAX_N + 1];
+int ans[MAX_N + 1];
+unordered_set<int> s[MAX_N + 1];
 
-int seats[MAX_N+1];
-unordered_set<int> cnt[MAX_N+1];
-vector<pair<int, int>> exchange_pair;
+int main(){
+    // 입력:
+    cin >> n >> k;
+    for(int i = 0; i < k; i++)
+        cin >> a[i] >> b[i];
+    
+    // 시작 array를 arr에 표시합니다.
+    for(int i = 1; i <= n; i++) {
+        arr[i] = i;
+        // 각 i 숫자가 위치할 수 있는 자리를 HashSet에 넣어 저장합니다.
+        s[i].insert(i);
+        ans[i] = 1;
+    }
 
-int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		seats[i] = i;
-		cnt[seats[i]].insert(i);
-	}
-	for (int i = 0; i < k; i++) {
-		int a, b; cin >> a >> b;
-		exchange_pair.push_back({ a,b });
-		cnt[seats[a]].insert(b);
-		cnt[seats[b]].insert(a);
-		int tmp = seats[a];
-		seats[a] = seats[b];
-		seats[b] = tmp;
-	}
-	for (int i = 0; i < exchange_pair.size(); i++) {
-		int a = exchange_pair[i].first, b = exchange_pair[i].second;
-		cnt[seats[a]].insert(b);
-		cnt[seats[b]].insert(a);
-		int tmp = seats[a];
-		seats[a] = seats[b];
-		seats[b] = tmp;
-	}
-	for (int i = 0; i < exchange_pair.size(); i++) {
-		int a = exchange_pair[i].first, b = exchange_pair[i].second;
-		cnt[seats[a]].insert(b);
-		cnt[seats[b]].insert(a);
-		int tmp = seats[a];
-		seats[a] = seats[b];
-		seats[b] = tmp;
-	}
-	for (int i = 1; i <= n; i++) {
-		cout << cnt[i].size() << "\n";
-	}
+    // 3k번 자리를 바꿉니다.
+    for(int cnt = 0; cnt < 3; cnt++) {
+        for(int i = 0; i < k; i++) {
+            // a[i]와 b[i]의 자리를 바꿔줍니다.
+            // 이때 자리가 변화한 값은 arr[a[i]]와 arr[b[i]]밖에 없습니다.
+            // 이 값들만 새로운 자리를 HashSet에 넣어 저장합니다.
+            swap(arr[a[i]], arr[b[i]]);
+
+            if(s[arr[a[i]]].find(a[i]) == s[arr[a[i]]].end()) {
+                s[arr[a[i]]].insert(a[i]);
+                ans[arr[a[i]]]++;
+            }
+            
+            if(s[arr[b[i]]].find(b[i]) == s[arr[b[i]]].end()) {
+                s[arr[b[i]]].insert(b[i]);
+                ans[arr[b[i]]]++;
+            }
+        }
+    }
+    
+    // 3k번 자리를 바꾸면서 각 숫자들이 위치할 수 있는 값들의 개수를 ans 배열에서 확인합니다.
+    for(int i = 1; i <= n; i++)
+        cout << ans[i] << endl;
+    return 0;
 }
