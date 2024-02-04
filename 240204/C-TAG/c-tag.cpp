77ddcb28adf351c032d1b252f8cd7773528d8f56
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-04 13:41:02 +0900

    Title: C-TAG, Time: 1178ms, Memory: 0MB, Status: Passed - CodeTree

--- 240204/C-TAG/c-tag.cpp
+++ 240204/C-TAG/c-tag.cpp
@@ -3,51 +3,53 @@
 using namespace std;
 
 #define MAX_N 500
-#define MAX_M 50
 
+// 변수 선언
 int n, m;
+int ans;
+string A[MAX_N], B[MAX_N];
+unordered_set<string> s;
+
+bool TestLocation(int x, int y, int z) {
+    // x, y, z번째 자릿수를 선택했을 때 A와 B 그룹이
+    // 완벽하게 구분되면 true, 그렇지 않다면 false를 반환합니다.
+    s.clear();
+
+    // A의 원소를 전부 HashSet에 넣어줍니다.
+    for(int i = 0; i < n; i++) {
+        s.insert(A[i].substr(x, 1) + A[i].substr(y, 1) + A[i].substr(z, 1));
+    }
+
+    // B의 원소 중 하나라도 A와 같은 경우가 있다면
+    // A와 B를 구분해낼 수 없습니다.
+    for(int i = 0; i < n; i++) {
+        if(s.find(B[i].substr(x, 1) + B[i].substr(y, 1) + B[i].substr(z, 1)) != s.end())
+            return false;
+    }
+
+    // 모든 B의 원소가 A와 다르다면 A와 B를 구분해낼 수 있습니다.
+    return true;
+}
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	cin >> n >> m;
-	char arrA[MAX_N][MAX_N];
-	char arrB[MAX_N][MAX_N];
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arrA[i][j];
-		}
-		cin.ignore();
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arrB[i][j];
-		}
-		cin.ignore();
-	}
+    // 입력:
+    cin >> n >> m;
 
-	int ans = 0;
-	for (int col1 = 0; col1 < m-2; col1++) {
-		for (int col2 = col1 + 1; col2 < m - 1; col2++) {
-			for (int col3 = col2 + 1; col3 < m; col3++) {
-				int is_possible = true;
-				string arrsetA[MAX_N];
-				unordered_set<string> set2;
-				for (int row = 0; row < n; row++) {
-					string s = string(1, arrA[row][col1]) + string(1, arrA[row][col2]) + string(1, arrA[row][col3]);
-					arrsetA[row] = s;
-					s = string(1, arrB[row][col1]) + string(1, arrB[row][col2]) + string(1, arrB[row][col3]);
-					set2.insert(s);
-				}
-				for (int i = 0; i < n; i++) {
-					if (set2.find(arrsetA[i]) != set2.end()) {
-						is_possible = false;
-						break;
-					}
-				}
-				if(is_possible) ans++;
-			}
-		}
-	}
-	
-	cout << ans << "\n";
+    for(int i = 0; i < n; i++)
+        cin >> A[i];
+    
+    for(int i = 0; i < n; i++)
+        cin >> B[i];
+
+    // 서로 다른 세 자리의 조합을 모두 순회합니다.
+    for (int i = 0; i < m; i++)
+        for (int j = i + 1; j < m; j++)
+            for (int k = j + 1; k < m; k++)
+                // i, j, k 번째 자리를 선택했을 때 두 그룹을
+                // 완벽하게 구분할 수 있는지 확인합니다.
+                if (TestLocation(i, j, k)) ans++;
+    
+    // 두 그룹을 구분해낼 수 있는 조합 수를 출력합니다.
+    cout << ans;
+    return 0;
 }
