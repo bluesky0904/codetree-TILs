45958b7f741e06b95f2867c8b3f4405b57a1db1c
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-04 10:51:05 +0900

    Title: 정수 찾기, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240204/정수 찾기/find-an-integer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+#define MAX_NUM 100000
+int n, m;
+int arr1[MAX_NUM];
+int arr2[MAX_NUM];
+unordered_set<int> set1;
+unordered_set<int> set2;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr1[i];
+		set1.insert(arr1[i]);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> arr2[i];
+		set2.insert(arr2[i]);
+	}
+
+	for (int i = 0; i < m; i++) {
+		if (set1.find(arr2[i]) == set1.end()) cout << 0 << "\n";
+		else cout << 1 << "\n";
+	}
+}
