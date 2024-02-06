692f07f3126bb99374790f2f6c23e3377ca8e914
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-06 14:38:08 +0900

    Title: 벽이 없는 충돌 실험, Time: 295ms, Memory: 120MB, Status: Memory Limit Exceed - CodeTree

--- 240206/벽이 없는 충돌 실험/collision-experiment-without-wall.cpp
+++ 240206/벽이 없는 충돌 실험/collision-experiment-without-wall.cpp
@@ -11,7 +11,6 @@
 using namespace std;
 
 pair<int, int> nxtBoard[MAX_BOARD+1][MAX_BOARD+1];
-//            x    y    d    w   idx
 vector<tuple<int, int, int, int, int>> marbles;
 vector<tuple<int, int, int, int, int>> tmp;
 int mapper[ASCII_NUM];
@@ -26,10 +25,6 @@ bool InRange(int x, int y){
 }
 
 void MoveAll(){
-    // 1. Marble vector에서 순회하며, nxtBoard에 업데이트한다.
-        // 1.1 순회하며, vector도 업데이트하고, nxtBoard에는 값을 넣는다
-        // 1.2 nxtBoard가 0이 아닌경우, ans = g_time
-        // 1.3 무게 및 번호를 비교해서 nxtBoard를 업데이트한다
     vector<tuple<int, int, int, int, int>> tmp;
     int x, y, dir, w, idx;
     for(int i = 0 ; i < marbles.size() ; i++){
@@ -41,10 +36,8 @@ void MoveAll(){
             nxtBoard[nx][ny] = {w, idx};
         }else{
             ans = g_time;
-            // 무게 비교
             if(w > nxtBoard[nx][ny].X){
                 nxtBoard[nx][ny] = {w, idx};
-                // 번호 비교
             }else if(w == nxtBoard[nx][ny].X && idx > nxtBoard[nx][ny].Y){
                 nxtBoard[nx][ny] = {w, idx};
             }
@@ -54,10 +47,6 @@ void MoveAll(){
 }
 
 void Update(){
-    // 2. Update된 Marble vector를 순회하며, nxtBoard와 비교한다
-        // 2.1 tmp vector 하나 생성
-        // 2.2 비교해서 같으면 nxtBoard를 {0,0}으로 초기화, tmp에 push_back
-        // 2.3 다르면 continue;
     tmp.clear();
     for(int i = 0 ; i < marbles.size() ; i++){
         int x, y, dir, w, idx;
@@ -71,15 +60,6 @@ void Update(){
 }
 
 void Simulate(){
-    // // nxtBoard update
-    // Clear();
-    // 모든 board를 순회하며, marble nxt Board에 update O(B^2), Simulate가 B만큼 돌고 있어서, 
-    // O(B^3)만큼 걸린다. B는 약 4*10^3 이므로, 64*10^9정도. 64억개정도의 명령어... 64초정도 걸리게 되는것이다. 이 방법은 포기.
-    
-    // MoveAll();
-    // // nxtBoard를 탐색하며, board update
-    // Update();
-    
     MoveAll();
     Update();
 }
