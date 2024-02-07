5b5738f27964e7a9991fea89ce99bc09fb89a2f1
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-07 21:57:48 +0900

    Title: 최단 Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240207/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string A;
+int length_A;
+
+void Rotate(int cnt) {
+	while (cnt--) {
+		int temp = A[length_A - 1];
+		for (int i = length_A - 1; i >= 1; i--) {
+			A[i] = A[i - 1];
+		}
+		A[0] = temp;
+	}
+}
+
+int RunLengthEncoding() {
+	int cnt = 1;
+	string s;
+	for (int i = 0; i < length_A; i++) {
+		if (i + 1 < length_A && A[i] == A[i + 1]) cnt++;
+		else {
+			s += A[i];
+			s += to_string(cnt);
+			cnt = 1;
+		}
+	}
+	return s.length();
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> A;
+	length_A = A.length();
+
+	int min_length = 10;
+	for (int cnt = 1; cnt <= A.length(); cnt++) {
+		Rotate(cnt);
+		min_length = min(min_length, RunLengthEncoding());
+	}
+	cout << min_length << "\n";
+}
