731aefc27ce8060a37b88100b00b9b8e98cf66c3
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-09 22:56:52 +0900

    Title: 자리 차지하기, Time: 95ms, Memory: 5MB, Status: Passed - CodeTree

--- 240209/자리 차지하기/take-place.cpp
+++ 240209/자리 차지하기/take-place.cpp
@@ -1,39 +1,53 @@
-/*
-3 4
-4 1 1
-
-1 2 3 4
-
-*/
 #include <iostream>
 #include <set>
+
+#define MAX_N 100000
+
 using namespace std;
 
+// 변수 선언
 int n, m;
-set<int> s;
+int arr[MAX_N];
+
+set<int> seats;
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	cin >> n >> m;
-	
-	for (int i = 1; i <= m; i++) {
-		s.insert(i);
-	}
+    // 입력:
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    // 1번부터 m번까지 전부 비어져 있으므로
+    // 빈 위치를 treeset으로 관리합니다.
+    for(int i = 1; i <= m; i++)
+        seats.insert(i);
 
-	int cnt = 0;
-	for (int i = 0; i < n; i++) {
-		int x; cin >> x;
-		auto it = s.upper_bound(x);
-		if (it == s.begin()) {
-			cout << cnt << "\n";
-			return 0;
-		}
-		else {
-			it--;
-			s.erase(it);
-			cnt++;
-		}
-	}
-	cout << cnt << "\n";
-	return 0;
+    // 답을 구해줍니다.
+    int ans = 0;
+
+    // 순서대로 앉혀봅니다.
+    // 최선의 자리 선택은
+    // ai보다 같거나 작은 최대 위치에 자리배치를 하는 것입니다.
+    for(int i = 0; i < n; i++) {
+        // ai보다 큰 최초의 위치를 먼저 찾습니다.
+        set<int>::iterator idx = seats.upper_bound(arr[i]);
+        
+        // 만약 큰 최초의 위치가 
+        // 첫 번째 위치가 아니라면,
+        // 바로 전 위치가
+        // ai보다 같거나 작은 최대 위치가 구해지므로,
+        // 해당 위치에 사람을 앉혀줍니다.
+        if(idx != seats.begin()) {
+            idx--;
+            seats.erase(*idx);
+            
+            // 답을 갱신해줍니다.
+            ans++;
+        }
+        else
+            break;
+    }
+
+    cout << ans;
+    return 0;
 }
