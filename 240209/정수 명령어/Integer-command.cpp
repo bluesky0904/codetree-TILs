fcc122d7cee331bce4debe5f46b8b290fad20cb9
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-09 13:55:17 +0900

    Title: 정수 명령어, Time: 46ms, Memory: 0MB, Status: Passed - CodeTree

--- 240209/정수 명령어/Integer-command.cpp
+++ 240209/정수 명령어/Integer-command.cpp
@@ -1,42 +1,26 @@
 #include <iostream>
 #include <set>
-
 using namespace std;
 
-// 변수 선언
-int t;
-int n;
+int t, n;
+set<int> s;
 
 int main() {
-    // 입력:
-    cin >> t;
-    while(t--) {
-        set<int> s;
-        cin >> n;
-        
-        for(int i = 0; i < n; i++) {
-            char command; int x;
-            cin >> command >> x;
-
-            // treeset에 넣어줍니다.
-            if(command == 'I')
-                s.insert(x);
-            // 큐가 비어있지 않을 경우에는 값을 제거해줍니다.
-            else if(command == 'D' && (int) s.size() > 0) {
-                // x가 1이면 최댓값을 삭제합니다.
-                if(x == 1)
-                    s.erase(*s.rbegin());
-                // x가 -1이면 최솟값을 삭제합니다.
-                else
-                    s.erase(*s.begin());
-            }
-        }
-
-        if((int) s.size() == 0)
-            cout << "EMPTY" << endl;
-        else
-            cout << *s.rbegin() << " " << *s.begin() << endl;
-    }
-
-    return 0;
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> t;
+	while (t--) {
+		s.clear();
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			char command; int x;
+			cin >> command >> x;
+			if (command == 'I') s.insert(x);
+			else if (command == 'D' && s.size() > 0) {
+				if (x == 1) s.erase(*s.rbegin());
+				else s.erase(*s.begin());
+			}
+		}
+		if (s.size() == 0) cout << "EMPTY" << "\n";
+		else cout << *s.rbegin() << " " << *s.begin() << "\n";
+	}
 }
