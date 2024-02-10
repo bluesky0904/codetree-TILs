a4331e564e590b51360f611855797b509d014f5c
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-10 11:52:09 +0900

    Title: 아름다운 수, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 240210/아름다운 수/beautiful-number.cpp
+++ 240210/아름다운 수/beautiful-number.cpp
@@ -1,34 +1,50 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+
 using namespace std;
 
 int n;
-int cnt;
-vector<int> ans;
+int ans;
+vector<int> seq;
 
-void Choose() {
-	if (ans.size() >= n) {
-		if (ans.size() == n) cnt++;
-		return;
+bool IsBeautiful() {
+    // 연달아 같은 숫자가 나오는 시작 위치를 잡습니다.
+	for(int i = 0; i < n; i += seq[i]) {
+        // 만약 연속하여 해당 숫자만큼 나올 수 없다면
+        // 아름다운 수가 아닙니다.
+		if(i + seq[i] - 1 >= n)
+            return false;
+        // 연속하여 해당 숫자만큼 같은 숫자가 있는지 확인합니다.
+        // 하나라도 다른 숫자가 있다면
+        // 아름다운 수가 아닙니다.
+        for(int j = i; j < i + seq[i]; j++)
+            if(seq[j] != seq[i])
+                return false;
 	}
-
-	for (int i = 1; i <= 4; i++) {
-		for (int j = 1; j <= i; j++) {
-			ans.push_back(i);
-		}
-		Choose();
-		for (int j = 1; j <= i; j++) {
-			ans.pop_back();
-		}
-	}
-	return;
+	return true;
 }
 
+void CountBeautifulSeq(int cnt) {
+	if(cnt == n) {
+		if(IsBeautiful())
+			ans++;
+		return;
+	}
+	
+	for(int i = 1; i <= 4; i++) {
+		seq.push_back(i);
+		CountBeautifulSeq(cnt + 1);
+		seq.pop_back();
+	}
+}
+
+
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	cin >> n;
-	cnt = 0;
-	Choose();
-	cout << cnt << "\n";
+	
+	CountBeautifulSeq(0);
+	
+	cout << ans;
 	return 0;
 }
