8ed2152d357c3102a1afff7d037f532243f4e99a
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-11 15:05:25 +0900

    Title: 사다리 타기, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240211/사다리 타기/ladder-game.cpp
+++ 240211/사다리 타기/ladder-game.cpp
@@ -12,30 +12,30 @@ vector<pair<int, int>> lines;
 vector<pair<int, int>> selected_lines;
 
 bool Possible() {
-	int nums1[MAX_N], nums2[MAX_N];
+	int num1[MAX_N], num2[MAX_N];
 	for (int i = 0; i < n; i++) {
-		nums1[i] = nums2[i] = i;
+		num1[i] = num2[i] = i;
 	}
 
 	for (int i = 0; i < lines.size(); i++) {
 		int idx = lines[i].second;
-		swap(nums1[idx], nums1[idx+1]);
+		swap(num1[idx], num1[idx + 1]);
 	}
 
 	for (int i = 0; i < selected_lines.size(); i++) {
 		int idx = selected_lines[i].second;
-		swap(nums2[idx], nums2[idx + 1]);
+		swap(num2[idx], num2[idx + 1]);
 	}
 
 	for (int i = 0; i < n; i++) {
-		if (nums1[i] != nums2[i]) return false;
+		if (num1[i] != num2[i]) return false;
 	}
 	return true;
 }
 
 void FindMinLines(int cnt) {
 	if (cnt == m) {
-		if (Possible()) ans = min(ans, (int) selected_lines.size());
+		if (Possible()) ans = min(ans, (int)selected_lines.size());
 		return;
 	}
 
@@ -44,6 +44,7 @@ void FindMinLines(int cnt) {
 	selected_lines.pop_back();
 
 	FindMinLines(cnt + 1);
+	return;
 }
 
 int main() {
@@ -51,7 +52,7 @@ int main() {
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
 		int a, b; cin >> a >> b;
-		lines.push_back(make_pair(b, a-1));
+		lines.push_back(make_pair(b, a - 1));
 	}
 
 	sort(lines.begin(), lines.end());
