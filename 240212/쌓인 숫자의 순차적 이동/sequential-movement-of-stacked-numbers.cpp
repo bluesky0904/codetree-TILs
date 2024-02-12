e567188f1a7466f1aadf6b180b70acf89ced47c4
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-12 15:19:53 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240212/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.cpp
+++ 240212/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 #define MAX_N 20
 #define DIR_NUM 8
-#define NONE make_pair(-1,1)
-
+#define OUT_OF_GRID make_pair(-1,-1)
 int n, m;
+vector<int> grid[MAX_N][MAX_N];
+
 int dx[DIR_NUM] = {-1,-1,-1,0,1,1,1,0};
 int dy[DIR_NUM] = {1,0,-1,-1,-1,0,1,1};
-vector<int> grid[MAX_N][MAX_N];
 
 pair<int, int> GetNumPos(int num) {
 	for (int x = 0; x < n; x++) {
@@ -29,21 +29,21 @@ bool InRange(int x, int y) {
 pair<int, int> GetNextPos(pair<int, int> pos) {
 	int x, y;
 	tie(x, y) = pos;
-	int max_num = -1;
+	int max_val = -1;
 	int mx, my;
-	tie(mx, my) = NONE;
-	for (int i = 0; i < DIR_NUM; i++) {
-		int nx = x + dx[i], ny = y + dy[i];
+	pair<int, int> max_pos = OUT_OF_GRID;
+	for (int dir = 0; dir < DIR_NUM; dir++) {
+		int nx = x + dx[dir], ny = y + dy[dir];
 		if (InRange(nx, ny)) {
-			for (int j = 0; j < grid[nx][ny].size(); j++) {
-				if (grid[nx][ny][j] > max_num) {
-					mx = nx, my = ny;
-					max_num = grid[nx][ny][j];
+			for (int i = 0; i < grid[nx][ny].size(); i++) {
+				if (grid[nx][ny][i] > max_val) {
+					max_val = grid[nx][ny][i];
+					max_pos = make_pair(nx, ny);
 				}
 			}
 		}
 	}
-	return make_pair(mx, my);
+	return max_pos;
 }
 
 void Move(pair<int, int> pos, pair<int, int> next_pos, int num) {
@@ -58,18 +58,15 @@ void Move(pair<int, int> pos, pair<int, int> next_pos, int num) {
 		if (to_move) grid[nx][ny].push_back(grid[x][y][i]);
 	}
 
-	while (grid[x][y].back() != num) {
-		grid[x][y].pop_back();
-	}
+	while (grid[x][y].back() != num) grid[x][y].pop_back();
 	grid[x][y].pop_back();
 }
 
 void Simulate(int num) {
-	pair<int ,int> pos = GetNumPos(num);
+	pair<int, int> pos = GetNumPos(num);
 	pair<int, int> next_pos = GetNextPos(pos);
-	
-	if (next_pos != NONE) Move(pos, next_pos, num);
-}	
+	if (next_pos != OUT_OF_GRID) Move(pos, next_pos, num);
+}
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
@@ -80,18 +77,18 @@ int main() {
 			grid[i][j].push_back(num);
 		}
 	}
-	
+
 	for (int i = 0; i < m; i++) {
 		int num; cin >> num;
 		Simulate(num);
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (grid[i][j].size() == 0) cout << "None" << "\n";
+	for (int x = 0; x < n; x++) {
+		for (int y = 0; y < n; y++) {
+			if (grid[x][y].size() == 0) cout << "None" << "\n";
 			else {
-				for (int k = grid[i][j].size() - 1; k >= 0; k--) {
-					cout << grid[i][j][k] << " ";
+				for (int i = grid[x][y].size() - 1; i >= 0; i--) {
+					cout << grid[x][y][i] << " ";
 				}
 				cout << "\n";
 			}
