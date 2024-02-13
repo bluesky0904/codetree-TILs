1fbb63ac014f85a1f34df4d0329073369aac1cba
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-13 11:27:01 +0900

    Title: n개 중에 m개 뽑기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240213/n개 중에 m개 뽑기/n-choose-m.cpp
+++ 240213/n개 중에 m개 뽑기/n-choose-m.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int n, m;
-vector<int> ans;
+vector<int> combination;
 
-void Print() {
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i] << " ";
-	}
-	cout << "\n";
+// 방문한 원소들을 출력해줍니다.
+void PrintCombination() {
+    for(int i = 0; i < combination.size(); i++)
+        cout << combination[i] << " ";
+    cout << endl;
 }
 
-void Choose(int cur_num, int prev) {
-	if (cur_num == m) {
-		Print();
-		return;
-	}
+// 지금까지 뽑은 갯수와 마지막으로 뽑힌 숫자를 추적하여
+// 그 다음에 뽑힐 수 있는 원소의 후보를 정합니다.
+void FindCombination(int cnt, int last_num) {
+    // m개를 모두 뽑은 경우 답을 출력해줍니다.
+    if(cnt == m)  {
+        PrintCombination();
+        return;
+    }
 
-	for (int i = 1; i <= n; i++) {
-		if (prev < i) {
-			ans.push_back(i);
-			Choose(cur_num + 1, i);
-			ans.pop_back();
-		}
-	}
+    // 뽑을 수 있는 원소의 후보들을 탐색합니다.
+    for(int i = last_num + 1; i <= n; i++) { 
+        combination.push_back(i);
+        FindCombination(cnt + 1, i);
+        combination.pop_back();
+    }
 }
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	cin >> n >> m;
-	Choose(0, 0);
+    cin >> n >> m;
+
+    // 가능한 범위를 순회하며 해당 숫자가 
+    // 조합의 첫번째 숫자일 때를 탐색합니다.
+    for(int i = 1; i <= n; i++) {
+        combination.push_back(i);
+        FindCombination(1, i);
+        combination.pop_back();
+    }
+
+    return 0;
 }
