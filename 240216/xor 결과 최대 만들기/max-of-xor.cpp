6347563c85314e38072c8e4b82e562e9b580b543
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-16 20:40:38 +0900

    Title: xor 결과 최대 만들기, Time: 1025ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240216/xor 결과 최대 만들기/max-of-xor.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int ans;
+vector<int> v;
+vector<int> selected;
+
+int CalcXor() {
+	int sum = selected[0];
+	for (int i = 1; i < selected.size(); i++) {
+		sum ^= selected[i];
+	}
+	return sum;
+}
+
+void Choose(int cur_num, int prev) {
+	if (cur_num == m) {
+		ans = max(ans, CalcXor());
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (prev < v[i]) {
+			selected.push_back(v[i]);
+			Choose(cur_num + 1, v[i]);
+			selected.push_back(v[i]);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		v.push_back(x);
+	}
+	
+	sort(v.begin(), v.end());
+
+	Choose(0, 0);
+
+	cout << ans << "\n";
+}
