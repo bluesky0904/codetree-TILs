d6cff052c29853e1cd33aaae4d03cf1ebf32cb81
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-02-28 14:40:08 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 41ms, Memory: 2MB, Status: Passed - CodeTree

--- 240228/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
+++ 240228/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -1,42 +1,56 @@
-#include <stdio.h>
+#include <iostream>
 #include <queue>
+#include <tuple>
+#include <climits>
 using namespace std;
+
+#define MAX_N 1000000
+
 int n;
-int visit[1000005]; int step[1000005];
-queue<int>q;
-bool safe(int x) {
-  if(1<=x) return true;
-  return false;
+int min_cnt = INT_MAX;
+
+queue<pair<int, int>> q;
+bool visited[2 * MAX_N];
+
+bool InRange(int x) {
+	return 1 <= x && x <= 2 * n - 1;
 }
+
+int BFS() {
+	q.push(make_pair(n, 0));
+	visited[n] = true;
+	while (!q.empty()) {
+		int cx, cnt;
+		tie(cx, cnt) = q.front();
+		q.pop();
+
+		if (cx == 1) min_cnt = min(min_cnt, cnt);
+		
+		if (InRange(cx - 1) && !visited[cx - 1]) {
+			q.push(make_pair(cx - 1, cnt + 1));
+			visited[cx - 1] = true;
+		}
+
+		if (InRange(cx + 1) && !visited[cx + 1]) {
+			q.push(make_pair(cx + 1, cnt + 1));
+			visited[cx + 1] = true;
+		}
+
+		if (cx % 2 == 0 && InRange(cx / 2) && !visited[cx / 2]) {
+			q.push(make_pair(cx/2, cnt + 1));
+			visited[cx / 2] = true;
+		}
+
+		if (cx % 3 == 0 && InRange(cx / 3) && !visited[cx / 3]) {
+			q.push(make_pair(cx / 3, cnt + 1));
+			visited[cx / 3] = true;
+		}
+	}
+	return min_cnt;
+}
+
 int main() {
-  scanf("%d",&n);
-  visit[n]=1; q.push(n);
-  while(!q.empty()) {
-    if(q.front()==1) {
-      printf("%d",step[1]);
-      break;
-    }
-    if(safe(q.front()-1)&&!visit[q.front()-1]) {
-      visit[q.front()-1]=1;
-      q.push(q.front()-1);
-      step[q.front()-1]=step[q.front()]+1;
-    }
-    if(!visit[q.front()+1]) {
-      visit[q.front()+1]=1;
-      q.push(q.front()+1);
-      step[q.front()+1]=step[q.front()]+1;
-    }
-    if(q.front()%3==0&&!visit[q.front()/3]) {
-      visit[q.front()/3]=1;
-      q.push(q.front()/3);
-      step[q.front()/3]=step[q.front()]+1;
-    }
-    if(q.front()%2==0&&!visit[q.front()/2]) {
-      visit[q.front()/2]=1;
-      q.push(q.front()/2);
-      step[q.front()/2]=step[q.front()]+1;
-    }
-    q.pop();
-  }
-  return 0;
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n;
+	cout << BFS() << "\n";
 }
