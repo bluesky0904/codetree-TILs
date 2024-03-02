d098ec64d3dda48343f66f9142d17bdfbdb852b5
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-03 00:25:38 +0900

    Title: 배열에서 자리 바꾸기, Time: 386ms, Memory: 19MB, Status: Passed - CodeTree

--- 240303/배열에서 자리 바꾸기/switch-position-in-array.cpp
+++ 240303/배열에서 자리 바꾸기/switch-position-in-array.cpp
@@ -1,61 +1,63 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-void swapSubarrays(vector<int>& arr, int a, int b, int c, int d) {
-    // 인덱스 찾기
-    int startA = find(arr.begin(), arr.end(), a) - arr.begin();
-    int endB = find(arr.begin(), arr.end(), b) - arr.begin();
-    int startC = find(arr.begin(), arr.end(), c) - arr.begin();
-    int endD = find(arr.begin(), arr.end(), d) - arr.begin();
+int n, q;
 
-    // 교환할 부분배열의 길이 계산
-    int lenAB = endB - startA + 1;
-    int lenCD = endD - startC + 1;
+struct Node {
+	int data;
 
-    // 부분배열을 임시 벡터에 저장
-    vector<int> subAB(arr.begin() + startA, arr.begin() + endB + 1);
-    vector<int> subCD(arr.begin() + startC, arr.begin() + endD + 1);
+	Node* prev, * next;
 
-    // 교환할 부분배열 삭제
-    arr.erase(arr.begin() + startC, arr.begin() + endD + 1);
-    arr.erase(arr.begin() + startA, arr.begin() + endB + 1);
+	Node(int data) : data(data), prev(nullptr), next(nullptr) {};
+};
 
-    // 인덱스 조정
-    if(startA < startC) {
-        startC -= lenAB;
-    } else {
-        startA -= lenCD;
-    }
+unordered_map<int, Node*> nodes;
 
-    // 임시 벡터에서 부분배열을 원래 배열에 삽입
-    arr.insert(arr.begin() + startC, subAB.begin(), subAB.end());
-    arr.insert(arr.begin() + startA, subCD.begin(), subCD.end());
+void Connect(Node* s, Node* e) {
+	if (nullptr != s) s->next = e;
+	if (nullptr != e) e->prev = s;
+}
+
+void SwapSubarray(Node* a, Node* b, Node* c, Node* d) {
+	Node* a_prev = c->prev;
+	Node* b_next = d->next;
+	Node* c_prev = a->prev;
+	Node* d_next = b->next;
+	
+	if (b->next == c) {
+		a_prev = d;
+		d_next = a;
+	}
+	if (d->next == a) {
+		b_next = c;
+		c_prev = b;
+	}
+
+	Connect(a_prev, a);
+	Connect(b, b_next);
+	Connect(c_prev, c);
+	Connect(d, d_next);
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n >> q;
+	for (int i = 1; i <= n; i++) nodes[i] = new Node(i);
+	for (int i = 1; i < n; i++) Connect(nodes[i], nodes[i + 1]);
 
-    int n, q;
-    cin >> n >> q;
+	while (q--) {
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		SwapSubarray(nodes[a], nodes[b], nodes[c], nodes[d]);
+	}
 
-    vector<int> arr(n);
-    for (int i = 0; i < n; ++i) {
-        arr[i] = i + 1;
-    }
+	Node* cur = nodes[1];
+	while (nullptr != cur->prev) cur = cur->prev;
 
-    for (int i = 0; i < q; ++i) {
-        int a, b, c, d;
-        cin >> a >> b >> c >> d;
-        swapSubarrays(arr, a, b, c, d);
-    }
-
-    for (auto& num : arr) {
-        cout << num << " ";
-    }
-    cout << "\n";
-
-    return 0;
+	while (nullptr != cur) {
+		cout << cur->data << " ";
+		cur = cur->next;
+	}
+	cout << "\n";
 }
