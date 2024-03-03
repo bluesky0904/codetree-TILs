3c6fc5ef3175ea269309a65428e89e544d2fc083
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-03 13:44:34 +0900

    Title: 책꽂이 정리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240303/책꽂이 정리/bookshelf-clean.cpp
+++ 240303/책꽂이 정리/bookshelf-clean.cpp
@@ -22,8 +22,8 @@ bool empty(int i) {
 }
 
 void connect(Node* u, Node* v) {
-	u->next = v;
-	v->prev = u;
+	if (nullptr != u) u->next = v;
+	if (nullptr != v) v->prev = u;
 }
 
 Node* pop_front(int i) {
@@ -48,29 +48,28 @@ Node* pop_back(int i) {
 		if (nullptr != tails[i]) tails[i]->next = nullptr;
 		else heads[i] = nullptr;
 	}
-	
+
 	return ret;
 }
 
-void push_front(int i, Node* singleton) {
-	if (nullptr == heads[i]) heads[i] = tails[i] = singleton;
+void push_front(int i, Node* node) {
+	if (nullptr == heads[i]) heads[i] = tails[i] = node;
 	else {
-		connect(singleton, heads[i]);
-		heads[i] = singleton;
+		connect(node, heads[i]);
+		heads[i] = node;
 	}
 }
 
-void push_back(int i, Node* singleton) {
-	if (nullptr == tails[i]) heads[i] = tails[i] = singleton;
+void push_back(int i, Node* node) {
+	if (nullptr == tails[i]) heads[i] = tails[i] = node;
 	else {
-		connect(tails[i], singleton);
-		tails[i] = singleton;
+		connect(tails[i], node);
+		tails[i] = node;
 	}
 }
 
 void move_all_front(int i, int j) {
 	if (i == j || empty(i)) return;
-
 	if (empty(j)) {
 		heads[j] = heads[i];
 		tails[j] = tails[i];
@@ -85,7 +84,6 @@ void move_all_front(int i, int j) {
 
 void move_all_back(int i, int j) {
 	if (i == j || empty(i)) return;
-
 	if (empty(j)) {
 		heads[j] = heads[i];
 		tails[j] = tails[i];
@@ -101,48 +99,42 @@ void move_all_back(int i, int j) {
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	cin >> n >> k >> q;
-
 	for (int i = 1; i <= n; i++) nodes[i] = new Node(i);
 	for (int i = 1; i < n; i++) connect(nodes[i], nodes[i + 1]);
-
 	for (int i = 1; i <= k; i++) heads[i] = tails[i] = nullptr;
 
-	heads[1] = nodes[1];
-	tails[1] = nodes[n];
-
 	while (q--) {
-		int type, i, j;
-		cin >> type >> i >> j;
-
-		if (type == 1) {
+		int command, i, j;
+		cin >> command >> i >> j;
+		if (command == 1) {
 			Node* node = pop_front(i);
 			if (nullptr != node) push_back(j, node);
 		}
-		else if (type == 2) {
+		else if (command == 2) {
 			Node* node = pop_back(i);
 			if (nullptr != node) push_front(j, node);
 		}
-		else if (type == 3) {
+		else if (command == 3) {
 			move_all_front(i, j);
 		}
-		else if (type == 4) {
+		else if (command == 4) {
 			move_all_back(i, j);
 		}
 	}
 
 	for (int i = 1; i <= k; i++) {
 		int cnt = 0;
+
 		Node* cur = heads[i];
 		while (nullptr != cur) {
 			cnt++;
 			cur = cur->next;
 		}
-
-		cout << cnt;
+		cout << cnt << " ";
 
 		cur = heads[i];
 		while (nullptr != cur) {
-			cout << " " << cur->data;
+			cout << cur->data << " ";
 			cur = cur->next;
 		}
 
@@ -153,6 +145,6 @@ int main() {
 		delete nodes[i];
 		nodes[i] = nullptr;
 	}
-	
+
 	return 0;
 }
