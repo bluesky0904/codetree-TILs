b7aa5320d126cb748215fbc75b7b89de99ee5b62
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-04 16:59:34 +0900

    Title: 기사 출정식, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240304/기사 출정식/knights-commencement.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 100000
+
+int n, m;
+
+struct Node {
+	int data;
+
+	Node* prev, * next;
+
+	Node(int data) : data(data), prev(nullptr), next(nullptr) {};
+};
+
+Node* nodes[MAX_N + 1];
+
+void connect(Node* s, Node* e) {
+	if (s) s->next = e;
+	if (e) e->prev = s;
+}
+
+void pop(Node* node) {
+	connect(node->prev, node->next);
+	node->prev = node->next = nullptr;
+}
+
+void print_node(Node* node) {
+	cout << node->next->data << " " << node->prev->data << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+
+	Node* prev = nullptr;
+	Node* cur = nullptr;
+
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+		Node* target = new Node(num);
+		nodes[num] = target;
+		if (i == 1) cur = target;
+		else connect(prev, target);
+
+		if (i == n) connect(target, cur);
+
+		prev = target;
+	}
+
+	while (m--) {
+		int num;
+		cin >> num;
+		print_node(nodes[num]);
+		pop(nodes[num]);
+	}
+
+	return 0;
+}
