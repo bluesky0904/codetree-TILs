e6232ba95ca8a9851aa4de332dd174ea992fc9bc
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-04 16:04:21 +0900

    Title: 테디의 여행 플래너, Time: 48ms, Memory: 8MB, Status: Passed - CodeTree

--- 240304/테디의 여행 플래너/teddys-travel-planner.cpp
+++ 240304/테디의 여행 플래너/teddys-travel-planner.cpp
@@ -1,84 +1,78 @@
 #include <iostream>
-
 using namespace std;
 
-// 한 노드를 나타내는 구조체입니다.
-struct Node {
-    // 문자열을 값으로 가집니다.
-    string data;
-    Node *prev, *next;
+int n, q;
 
-    Node(string data) : data(data), prev(nullptr), next(nullptr) {};
+struct Node {
+	string name;
+	Node* prev, * next;
+
+	Node(string name) : name(name), prev(nullptr), next(nullptr) {};
 };
 
-// 두 도시를 연결해줍니다.
-void connect(Node *s, Node *e) {
-    if (nullptr != s) s->next = e;
-    if (nullptr != e) e->prev = s;
+void connect(Node* u, Node* v) {
+	if (nullptr != u) u->next = v;
+	if (nullptr != v) v->prev = u;
 }
 
-// target 뒤에 s를 삽입합니다.
-void insertNext(Node *target, Node *s) {
-    connect(s, target->next);
-    connect(target, s);
+void push_next(Node* pinset, Node* node) {
+	connect(node, pinset->next);
+	connect(pinset, node);
 }
 
-// 해당 도시를 삭제합니다.
-void pop(Node *u) {
-    connect(u->prev, u->next);
-    u->prev = u->next = nullptr;
+void remove(Node* node) {
+	connect(node->prev, node->next);
+	node->prev = node->next = nullptr;
+}
+
+void print_node(Node* cur) {
+	if (cur->prev == nullptr || cur->next == nullptr || (cur->prev->name == cur->next->name)) {
+		cout << -1 << "\n";
+		return;
+	}
+	cout << cur->prev->name << " " << cur->next->name << "\n";
 }
 
 int main() {
-    int n, q;
-    cin >> n >> q;
-    Node *cur, *prev;
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n >> q;
+	Node* cur = nullptr;
+	Node* prev = nullptr;
 
-    // 지구본이라 연결 리스트가 원형으로 이어져 있음에 유의합니다.
-    for (int i = 1; i <= n; i++) {
-        string x;
-        cin >> x;
-        Node *target = new Node(x);
+	for (int i = 1; i <= n; i++) {
+		string s;
+		cin >> s;
+		Node* target = new Node(s);
 
-        // 맨 첫 번째 도시가 핀셋으로 꽂혀 있는 도시입니다.
-        if (i == 1) {
-            cur = target;
-        } else {
-            // 그 전 도시들과 이어줍니다.
-            connect(prev, target);
-        }
-        // 원형이기 때문에 마지막 도시는 핀셋이 꽂혀 있는 도시와 연결해줍니다.
-        if (i == n) connect(target, cur);
-        // 이전 도시들과 연결해주기 위함입니다.
-        prev = target;
-    }
+		if (i == 1) cur = target;
+		else connect(prev, target);
 
-    // q 개의 행동을 진행합니다.
-    while (q--) {
-        int option;
-        cin >> option;
+		if (i == n) connect(target, cur);
 
-        if (option == 1) {
-            if (cur->next) cur = cur->next;
-        }
+		prev = target;
+	}
 
-        if (option == 2) {
-            if (cur->prev) cur = cur->prev;
-        }
+	while (q--) {
+		int command;
+		cin >> command;
 
-        if (option == 3) {
-            if (cur->next != cur)
-                pop(cur->next);
-        }
+		if (command == 1) {
+			if (cur->next) cur = cur->next;
+		}
+		else if (command == 2) {
+			if (cur->prev) cur = cur->prev;
+		}
+		else if (command == 3) {
+			remove(cur->next);
+		}
+		else if (command == 4) {
+			string s;
+			cin >> s;
+			push_next(cur, new Node(s));
+		}
 
-        if (option == 4) {
-            string x;
-            cin >> x;
-            insertNext(cur, new Node(x));
-        }
+		print_node(cur);
+	}
 
-        // 문제의 조건대로 출력해줍니다.
-        if (cur->next == cur->prev || cur->next == nullptr || cur->prev == nullptr) cout << -1 << "\n";
-        else cout << cur->prev->data << " " << cur->next->data << "\n";
-    }
+	return 0;
 }
