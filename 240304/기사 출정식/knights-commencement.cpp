fa76d3609d94572149044c0ff1e85439fdfa2f61
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-04 17:18:56 +0900

    Title: 기사 출정식, Time: 108ms, Memory: 8MB, Status: Passed - CodeTree

--- 240304/기사 출정식/knights-commencement.cpp
+++ 240304/기사 출정식/knights-commencement.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 #define MAX_N 100000
@@ -6,69 +7,56 @@ using namespace std;
 int n, m;
 
 struct Node {
-    int data;
-    Node* prev, * next;
-    Node(int data) : data(data), prev(nullptr), next(nullptr) {};
+	int data;
+
+	Node* prev, * next;
+
+	Node(int data) : data(data), prev(nullptr), next(nullptr) {};
 };
 
-Node* nodes[MAX_N + 1];
+unordered_map<int, Node*> nodes;
 
 void connect(Node* s, Node* e) {
-    if (s) s->next = e;
-    if (e) e->prev = s;
+	if (s) s->next = e;
+	if (e) e->prev = s;
 }
 
 void pop(Node* node) {
-    if (node->next == node) { // 리스트에 노드가 하나만 있을 경우
-        nodes[node->data] = nullptr; // 해당 노드를 배열에서 제거
-    } else {
-        connect(node->prev, node->next);
-        if (nodes[node->data] == node) {
-            nodes[node->data] = nullptr;
-        }
-    }
-    delete node; // 메모리 해제
+	connect(node->prev, node->next);
+	node->prev = node->next = nullptr;
 }
 
 void print_node(Node* node) {
-    if (node == nullptr || node->next == nullptr || node->prev == nullptr) {
-        cout << "Error: Node is isolated or does not exist." << "\n";
-        return;
-    }
-    cout << node->next->data << " " << node->prev->data << "\n";
+	cout << node->next->data << " " << node->prev->data << "\n";
 }
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    cin >> n >> m;
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
 
-    Node* prev = nullptr;
-    Node* cur = nullptr;
+	Node* prev = nullptr;
+	Node* cur = nullptr;
 
-    for (int i = 1; i <= n; i++) {
-        int num;
-        cin >> num;
-        Node* target = new Node(num);
-        nodes[num] = target;
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+		Node* target = new Node(num);
+		nodes[num] = target;
 
-        if (i == 1) cur = target;
-        else connect(prev, target);
+		if (i == 1) cur = target;
+		else connect(prev, target);
 
-        if (i == n && n > 1) connect(target, cur); // 순환 구조 생성
+		if (i == n) connect(target, cur);
 
-        prev = target;
-    }
+		prev = target;
+	}
 
-    while (m--) {
-        int num;
-        cin >> num;
-        if (nodes[num] != nullptr) {
-            print_node(nodes[num]);
-            pop(nodes[num]);
-        } else {
-            cout << "Node does not exist or has been removed." << "\n";
-        }
-    }
+	while (m--) {
+		int num;
+		cin >> num;
+		print_node(nodes[num]);
+		pop(nodes[num]);
+	}
 
-    return 0;
+	return 0;
 }
