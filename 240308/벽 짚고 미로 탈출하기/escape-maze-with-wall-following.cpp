8dcc78f41fd677bc30bdc82c3a31235ea7a67ccc
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-08 20:16:50 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240308/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
+++ 240308/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <unordered_set>
-#include <tuple>
 using namespace std;
 
 #define MAX_N 100
@@ -8,11 +6,10 @@ using namespace std;
 
 int n, x, y;
 char grid[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N][DIR_NUM];
 
 int dx[DIR_NUM] = {0, -1, 0, 1};
 int dy[DIR_NUM] = {1, 0, -1, 0};
-
-bool visited[MAX_N][MAX_N][DIR_NUM];
 
 bool InRange(int x, int y) {
 	return 0 <= x && x < n && 0 <= y && y < n;
@@ -25,7 +22,7 @@ int Simulate(int x, int y) {
 	while (true) {
 		int nx = cx + dx[dir], ny = cy + dy[dir];
 
-		if (visited[nx][ny][dir]) return -1;
+		if (InRange(nx, ny) && visited[nx][ny][dir]) return -1;
 
 		if (InRange(nx, ny) && grid[nx][ny] == '#') {
 			dir = (dir + 1) % 4;
@@ -34,19 +31,22 @@ int Simulate(int x, int y) {
 			elapsed_time++;
 			return elapsed_time;
 		}
-		else if (InRange(nx, ny) && !visited[nx][ny][dir] && grid[nx][ny] == '.' && grid[nx + dx[(dir + 3) % 4]][ny + dy[(dir + 3) % 4]] == '#') {
-			elapsed_time++;
-			cx = nx;
-			cy = ny;
-			visited[cx][cy][dir] = true;
-		}
-		else if (InRange(nx, ny) && !visited[nx][ny][dir] && grid[nx][ny] == '.' && !visited[nx + dx[(dir + 3) % 4]][ny + dy[(dir + 3) % 4]][dir] && grid[nx + dx[(dir + 3) % 4]][ny + dy[(dir + 3) % 4]] == '.') {
-			elapsed_time += 2;
-			visited[nx][ny][dir] = true;
-			cx = nx + dx[(dir + 3) % 4];
-			cy = ny + dy[(dir + 3) % 4];
-			dir = (dir + 3) % 4;
-			visited[cx][cy][dir] = true;
+		else {
+			int rx = nx + dx[(dir + 3) % 4], ry = ny + dy[(dir + 3) % 4];
+			if (InRange(rx, ry) && grid[rx][ry] == '#') {
+				elapsed_time++;
+				cx = nx;
+				cy = ny;
+				visited[cx][cy][dir] = true;
+			}
+			else {
+				elapsed_time++;
+				visited[nx][ny][dir] = true;
+				cx = rx;
+				cy = ry;
+				dir = (dir + 3) % 4;
+				visited[cx][cy][dir] = true;
+			}
 		}
 	}
 }
