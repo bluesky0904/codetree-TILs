36de911c40915a58a62a48e135f0c3b56bc8cc9e
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-08 19:41:42 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 1080ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240308/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <unordered_set>
+#include <tuple>
+using namespace std;
+
+#define MAX_N 100
+#define DIR_NUM 4
+
+int n, x, y;
+char grid[MAX_N][MAX_N];
+
+int dx[DIR_NUM] = {0, -1, 0, 1};
+int dy[DIR_NUM] = {1, 0, -1, 0};
+
+bool visited[MAX_N][MAX_N][DIR_NUM];
+
+bool InRange(int x, int y) {
+	return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+int Simulate(int x, int y) {
+	int elapsed_time = 0;
+	int cx = x, cy = y, dir = 0;
+	visited[cx][cy][dir] = true;
+	while (true) {
+		int nx = cx + dx[dir], ny = cy + dy[dir];
+		if (visited[nx][ny][dir]) return -1;
+
+		if (InRange(nx, ny) && grid[nx][ny] == '#') {
+			dir = (dir + 1) % 4;
+		}
+		else if (!InRange(nx, ny)) {
+			elapsed_time++;
+			return elapsed_time;
+		}
+		else if (InRange(nx, ny) && !visited[nx][ny][dir] && grid[nx][ny] == '.' && grid[nx + dx[(dir + 3) % 4]][ny + dy[(dir + 3) % 4]] == '#') {
+			elapsed_time++;
+			cx = nx;
+			cy = ny;
+			visited[cx][cy][dir] = true;
+		}
+		else if (InRange(nx, ny) && !visited[nx][ny][dir] && grid[nx][ny] == '.' && !visited[nx + dx[(dir + 3) % 4]][ny + dy[(dir + 3) % 4]][dir] && grid[nx + dx[(dir + 3) % 4]][ny + dy[(dir + 3) % 4]] == '.') {
+			elapsed_time += 2;
+			cx = nx + dx[(dir + 3) % 4];
+			cy = ny + dy[(dir + 3) % 4];
+			dir = (dir + 3) % 4;
+			visited[cx][cy][dir] = true;
+			visited[nx + dx[(dir + 3) % 4]][ny + dy[(dir + 3) % 4]][dir] = true;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> n >> x >> y;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> grid[i][j];
+		}
+		cin.ignore();
+	}
+
+	cout << Simulate(x - 1, y - 1) << "\n";
+
+	return 0;
+}
