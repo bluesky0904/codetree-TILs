0cb0d0034baa000b0131d15c5be0f4ad90013416
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-08 16:54:46 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240308/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
+++ 240308/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -8,8 +8,8 @@ int n, r, c;
 
 int grid[MAX_N][MAX_N];
 
-int dx[DIR_NUM] = {-1,1,0,0};
-int dy[DIR_NUM] = {0,0,-1,1};
+int dx[DIR_NUM] = { -1,1,0,0 };
+int dy[DIR_NUM] = { 0,0,-1,1 };
 
 bool InRange(int x, int y) {
 	return 0 <= x && x < n && 0 <= y && y < n;
@@ -19,24 +19,23 @@ void Simulate(int x, int y) {
 	int cx = x, cy = y;
 	cout << grid[cx][cy] << " ";
 	while (true) {
-		bool possible = false;
-		for (int dir = 0; dir < DIR_NUM; dir++) {
+		bool is_possible = false;
+		for (int dir = 0; dir <= DIR_NUM; dir++) {
 			int nx = cx + dx[dir], ny = cy + dy[dir];
 			if (InRange(nx, ny) && grid[nx][ny] > grid[cx][cy]) {
-				possible = true;
+				is_possible = true;
 				cx = nx;
 				cy = ny;
 				cout << grid[cx][cy] << " ";
 				break;
 			}
 		}
-		if (!possible) return;
+		if (!is_possible) return;
 	}
 }
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-
 	cin >> n >> r >> c;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
@@ -44,6 +43,6 @@ int main() {
 		}
 	}
 
-	Simulate(r - 1, c - 1);
+	Simulate(r-1, c-1);
 	return 0;
 }
