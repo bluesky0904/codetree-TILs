5a0fe6f388ac19ded7f47643f59eea2760eda06e
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-11 14:55:28 +0900

    Title: 놀이기구 탑승, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/놀이기구 탑승/go-on-the-rides.cpp
+++ 240311/놀이기구 탑승/go-on-the-rides.cpp
@@ -1,93 +1,118 @@
 #include <iostream>
-#include <algorithm>
-using namespace std;
+#include <cmath>
+#include <tuple>
 
 #define MAX_N 20
+#define MAX_NUM 400
 #define DIR_NUM 4
+#define EMPTY 0
+
+using namespace std;
+
+// 칸의 상태
+typedef tuple<int, int, int, int> Cell;
 
 int n;
 
-int grid[MAX_N + 1][MAX_N + 1];
-int favorite_students[MAX_N * MAX_N + 1][5];
+int target_num[MAX_NUM + 1];
+bool friends[MAX_NUM + 1][MAX_NUM + 1];
 
-int dx[DIR_NUM] = {-1,0,1,0};
-int dy[DIR_NUM] = {0,1,0,-1};
+// 놀이 기구 탑승 번호
+int rides[MAX_N + 1][MAX_N + 1];
+
+int dx[DIR_NUM] = {-1, 1, 0, 0};
+int dy[DIR_NUM] = {0, 0, -1, 1};
 
 bool InRange(int x, int y) {
-	return 0 <= x && x < n && 0 <= y && y < n;
+    return 1 <= x && x <= n && 1 <= y && y <= n;
 }
 
-void Simulate(int num) {
-	int mx = -1, my = -1;
-	int max_favorite_students_cnt = -1;
-	int max_empty_cnt = -1;
-	for (int row = 0; row < n; row++) {
-		for (int col = 0; col < n; col++) {
-			if (grid[row][col] == 0) {
-				int favorite_students_cnt = 0;
-				int empty_cnt = 0;
-				for (int dir = 0; dir < DIR_NUM; dir++) {
-					int nx = row + dx[dir], ny = col + dy[dir];
-					if (InRange(nx, ny)) {
-						if (grid[nx][ny] == 0) empty_cnt++;
-						else {
-							for (int i = 1; i <= 4; i++) {
-								if (grid[nx][ny] == favorite_students[num][i]) {
-									favorite_students_cnt++;
-									break;
-								}
-							}
-						}
-					}
-				}
-				if (max_favorite_students_cnt < favorite_students_cnt || (max_favorite_students_cnt == favorite_students_cnt && max_empty_cnt < empty_cnt)) {
-					mx = row;
-					my = col;
-					max_favorite_students_cnt = favorite_students_cnt;
-					max_empty_cnt = empty_cnt;
-				}
-			}
-		}
-	}
-	if(mx != -1 && my != -1) grid[mx][my] = num;
+// num2가 num1이 좋아하는 친구인지 판단합니다.
+bool IsFriend(int num1, int num2) {
+    return friends[num1][num2];
+}
+
+// 우선순위에 따라 쉽게 계산하기 위해
+// (좋아하는 친구 수, 빈 칸의 수, -행 번호, -열 번호)
+// 순서대로 값을 넣어줍니다.
+Cell GetCurrCell(int num, int x, int y) {
+    int friend_cnt = 0, blank_cnt = 0;
+    for(int i = 0; i < DIR_NUM; i++) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if(!InRange(nx, ny)) 
+            continue;
+        
+        if(rides[nx][ny] == EMPTY)
+            blank_cnt++;
+        else if(IsFriend(num, rides[nx][ny]))
+            friend_cnt++;
+    }
+
+    return make_tuple(friend_cnt, blank_cnt, -x, -y);
+}
+
+void Move(int num) {
+    // Step1. 가장 우선순위가 높은 칸을 선택합니다.
+    Cell best_cell = make_tuple(0, 0, -(n + 1), -(n + 1));
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            if(rides[i][j] == EMPTY) {
+                Cell curr = GetCurrCell(num, i, j);
+
+                if(best_cell < curr)
+                    best_cell = curr;
+            }
+
+    // Step2. 해당 위치에 탑승합니다.
+    int x, y;
+    tie(ignore, ignore, x, y) = best_cell;
+    rides[-x][-y] = num;
+}
+
+// (x, y) 위치에 있는 학생의 점수를 계산합니다.
+int GetScore(int x, int y) {
+    int cnt = 0;
+    for(int i = 0; i < DIR_NUM; i++) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if(InRange(nx, ny) && 
+           IsFriend(rides[x][y], rides[nx][ny]))
+            cnt++;
+    }
+
+    return (int)pow(10, cnt - 1);
+}
+
+// 총 점수를 계산합니다.
+int GetTotalScore() {
+    int score = 0;
+    
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            score += GetScore(i, j);
+    
+    return score;
 }
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	cin >> n;
-	for (int i = 1; i <= n * n; i++) {
-		int student_num;
-		cin >> student_num;
-		for (int j = 1; j <= 4; j++) {
-			cin >> favorite_students[student_num][j];
-		}
+    cin >> n;
+    
+    for(int i = 1; i <= n * n; i++) {
+        cin >> target_num[i];
 
-		Simulate(student_num);
-	}
+        for(int j = 1; j <= 4; j++) {
+            int friend_num;
+            cin >> friend_num;
+            
+            // 현재 번호에 친구 번호를 표시해줍니다.
+            friends[target_num[i]][friend_num] = true;
+        }
+    }
 
-	int score = 0;
-	for (int row = 0; row < n; row++) {
-		for (int col = 0; col < n; col++) {
-			int cnt = 0;
-			for (int dir = 0; dir < DIR_NUM; dir++) {
-				int nx = row + dx[dir], ny = col + dy[dir];
-				if (InRange(nx, ny)) {
-					for (int i = 1; i <= 4; i++) {
-						if (grid[nx][ny] == favorite_students[grid[row][col]][i]) {
-							cnt++;
-							break;
-						}
-					}
-				}
-			}
-			if (cnt == 0) continue;
-			else if (cnt == 1) score += 1;
-			else if (cnt == 2) score += 10;
-			else if (cnt == 3) score += 100;
-			else if (cnt == 4) score += 1000;
-		}
-	}
-
-	cout << score << "\n";
-	return 0;
+    // 순서대로 학생들을 놀이기구에 탑승시킵니다.
+    for(int i = 1; i <= n * n; i++)
+        Move(target_num[i]);
+    
+    // 총 점수를 계산합니다.
+    int ans = GetTotalScore();
+    cout << ans;
 }
