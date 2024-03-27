99f8c7c5d1193dc403ead612a820724cbce9ad8d
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-03-27 20:51:06 +0900

    Title: 팩맨, Time: 50ms, Memory: 12MB, Status: Passed - CodeTree

--- 240327/팩맨/pacman.cpp
+++ 240327/팩맨/pacman.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 #define GRID_SIZE 4
 #define DIR_NUM 8
-
-int dx[DIR_NUM] = {-1,-1,0,1,1,1,0,-1};
-int dy[DIR_NUM] = {0,-1,-1,-1,0,1,1,1};
 
 int m, t;
 int r, c;
 pair<int, int> pacman;
 vector<int> monster[GRID_SIZE][GRID_SIZE];
 vector<int> next_monster[GRID_SIZE][GRID_SIZE];
-vector<int> egg[GRID_SIZE][GRID_SIZE];
 int ghost[GRID_SIZE][GRID_SIZE];
+vector<int> egg[GRID_SIZE][GRID_SIZE];
 
-int max_cnt = -1;
-vector<int> temp_pac_dir;
-vector<int> pac_dir;
+int max_cnt;
+vector<int> pacman_dir;
+vector<int> temp_pacman_dir;
 
-bool InRange(int x, int y) {
-	return 0 <= x && x < GRID_SIZE && 0 <= y && y < GRID_SIZE;
-}
+int dx[DIR_NUM] = {-1,-1,0,1,1,1,0,-1};
+int dy[DIR_NUM] = {0,-1,-1,-1,0,1,1,1};
 
 void InitEgg() {
 	for (int x = 0; x < GRID_SIZE; x++) {
@@ -33,7 +28,6 @@ void InitEgg() {
 	}
 }
 
-// 몬스터 복제
 void TryReplicate() {
 	InitEgg();
 	for (int x = 0; x < GRID_SIZE; x++) {
@@ -45,7 +39,10 @@ void TryReplicate() {
 	}
 }
 
-// 몬스터 이동
+bool InRange(int x, int y) {
+	return 0 <= x && x < GRID_SIZE && 0 <= y && y < GRID_SIZE;
+}
+
 void MoveAllMonster() {
 	for (int x = 0; x < GRID_SIZE; x++) {
 		for (int y = 0; y < GRID_SIZE; y++) {
@@ -59,13 +56,13 @@ void MoveAllMonster() {
 				int move_dir = monster[x][y][i];
 				int nx = x + dx[move_dir];
 				int ny = y + dy[move_dir];
-				if (!InRange(nx, ny) || make_pair(nx, ny) == pacman || ghost[nx][ny] > 0) {
+				if (!InRange(nx, ny) || make_pair(nx, ny) == pacman || ghost[nx][ny] != 0) {
 					bool moved = false;
-					for (int cnt = 1; cnt <= 7 && !moved; cnt++) {
+					for (int cnt = 1; cnt <= 7; cnt++) {
 						int next_dir = (move_dir + cnt) % 8;
 						nx = x + dx[next_dir];
 						ny = y + dy[next_dir];
-						if (InRange(nx, ny) && (make_pair(nx, ny) != pacman) && ghost[nx][ny] == 0) {
+						if (InRange(nx, ny) && make_pair(nx, ny) != pacman && ghost[nx][ny] == 0) {
 							next_monster[nx][ny].push_back(next_dir);
 							moved = true;
 							break;
@@ -73,9 +70,7 @@ void MoveAllMonster() {
 					}
 					if(!moved) next_monster[x][y].push_back(move_dir);
 				}
-				else {
-					next_monster[nx][ny].push_back(monster[x][y][i]);
-				}			
+				else next_monster[nx][ny].push_back(move_dir);
 			}
 		}
 	}
@@ -87,69 +82,63 @@ void MoveAllMonster() {
 	}
 }
 
-// 팩맨이 이동할 때, 각 방향으로 이동했을 때, 몬스터의 수를 세어서 최대값을 찾는다.
 int CountMonster() {
 	bool visited[GRID_SIZE][GRID_SIZE] = {false,};
 	int cnt = 0;
 	int cx = pacman.first, cy = pacman.second;
-	for (int dir = 0; dir < (int)temp_pac_dir.size(); dir++) {
-		cx += dx[temp_pac_dir[dir]];
-		cy += dy[temp_pac_dir[dir]];
+	for (int dir = 0; dir < (int)temp_pacman_dir.size(); dir++) {
+		cx += dx[temp_pacman_dir[dir]];
+		cy += dy[temp_pacman_dir[dir]];
 		if (!InRange(cx, cy)) return -1;
 		if (visited[cx][cy]) continue;
 		else {
 			visited[cx][cy] = true;
-			cnt += (int)monster[cx][cy].size();
+			if (monster[cx][cy].size() > 0) cnt += monster[cx][cy].size();
 		}
 	}
 	return cnt;
 }
 
-// 팩맨이 이동할 수 있는 모든 경우의 수를 찾는다.
 void FindMaxRoute(int idx) {
 	if (idx == 3) {
 		int tmp = CountMonster();
-		if (max_cnt < tmp) {
-			max_cnt = tmp;
-			pac_dir = temp_pac_dir;
+		if (max_cnt < CountMonster()) {
+			max_cnt = CountMonster();
+			pacman_dir = temp_pacman_dir;
 		}
 		return;
 	}
 
 	for (int dir = 0; dir < DIR_NUM; dir += 2) {
-		temp_pac_dir.push_back(dir);
+		temp_pacman_dir.push_back(dir);
 		FindMaxRoute(idx + 1);
-		temp_pac_dir.pop_back();
+		temp_pacman_dir.pop_back();
 	}
 }
 
-// 팩맨 이동
 void MovePacman() {
-	pac_dir.clear();
+	int cx = pacman.first, cy = pacman.second;
 	max_cnt = -1;
 	FindMaxRoute(0);
-	int cx = pacman.first, cy = pacman.second;
-	for (int dir = 0; dir < (int)pac_dir.size(); dir++) {
-		cx += dx[pac_dir[dir]];
-		cy += dy[pac_dir[dir]];
+	for (int dir = 0; dir < (int)pacman_dir.size(); dir++) {
+		cx += dx[pacman_dir[dir]];
+		cy += dy[pacman_dir[dir]];
 		if ((int)monster[cx][cy].size() > 0) {
 			monster[cx][cy].clear();
 			ghost[cx][cy] = 3;
 		}
 	}
-	pacman = make_pair(cx, cy);
+	pacman = {cx, cy};
 }
 
-// 시체 제거
 void RemoveGhost() {
 	for (int x = 0; x < GRID_SIZE; x++) {
 		for (int y = 0; y < GRID_SIZE; y++) {
-			if(ghost[x][y] > 0) ghost[x][y]--;
+			if (ghost[x][y] > 0) ghost[x][y]--;
 		}
 	}
 }
 
-// 알 부화
 void CompleteReplicate() {
 	for (int x = 0; x < GRID_SIZE; x++) {
 		for (int y = 0; y < GRID_SIZE; y++) {
@@ -160,62 +149,19 @@ void CompleteReplicate() {
 	}
 }
 
-// 디버깅용 출력
-void Print() {
-	cout << "Pacman: " << pacman.first << " " << pacman.second << "\n";
-	for (int x = 0; x < GRID_SIZE; x++) {
-		for (int y = 0; y < GRID_SIZE; y++) {
-			if ((int)monster[x][y].size()) {
-				cout << "Monster: " << x << " " << y << "\n";
-				for (int i = 0; i < (int)monster[x][y].size(); i++) {
-					cout << monster[x][y][i] << " ";
-				}
-
-				cout << "\n";
-			}
-		}
-	}
-	cout << "Ghost\n";
-	for (int x = 0; x < GRID_SIZE; x++) {
-		for (int y = 0; y < GRID_SIZE; y++) {
-			cout << ghost[x][y] << " ";
-		}
-		cout << "\n";
-	}
-	cout << "Egg\n";
-	for (int x = 0; x < GRID_SIZE; x++) {
-		for (int y = 0; y < GRID_SIZE; y++) {
-			if ((int)egg[x][y].size()) {
-				cout << "Egg: " << x << " " << y << "\n";
-				for (int i = 0; i < (int)egg[x][y].size(); i++) {
-					cout << egg[x][y][i] << " ";
-				}
-			}
-		}
-		cout << "\n";
-	}
-	cout << "--------------------\n";
-}
-
 void Simulate() {
-	// 복제 시도
 	TryReplicate();
-	// 몬스터 이동
 	MoveAllMonster();
-	// 팩맨 이동
 	MovePacman();
-	// 시체 제거
 	RemoveGhost();
-	// 알 부화
 	CompleteReplicate();
-
 }
 
 int main() {
-	ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> m >> t;
 	cin >> r >> c;
-	pacman = make_pair(r - 1, c - 1);
+	pacman = {r - 1, c - 1};
 	for (int i = 0; i < m; i++) {
 		int r, c, d;
 		cin >> r >> c >> d;
@@ -227,7 +173,7 @@ int main() {
 	int ans = 0;
 	for (int x = 0; x < GRID_SIZE; x++) {
 		for (int y = 0; y < GRID_SIZE; y++) {
-			ans += (int)monster[x][y].size();
+			if(monster[x][y].size() > 0) ans += monster[x][y].size();
 		}
 	}
 	cout << ans << "\n";
