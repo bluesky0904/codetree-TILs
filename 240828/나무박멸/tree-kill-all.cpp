2b37b94dc4863faf4fdec90a07c7afca80f41f39
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-08-28 21:46:22 +0900

    Title: 나무박멸, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240828/나무박멸/tree-kill-all.cpp
+++ 240828/나무박멸/tree-kill-all.cpp
@@ -14,166 +14,153 @@ int next_grid[MAX_N][MAX_N];
 int herbicide[MAX_N][MAX_N];
 int ans = 0;
 
-int dx[DIR_NUM] = { -1, 0, 1, 0};
-int dy[DIR_NUM] = { 0, 1, 0, -1};
+int dx[DIR_NUM] = { -1, 0, 1, 0 };
+int dy[DIR_NUM] = { 0, 1, 0, -1 };
 
 int dx_diag[DIAG_DIR_NUM] = { -1, -1, 1, 1 };
 int dy_diag[DIAG_DIR_NUM] = { -1, 1, 1, -1 };
 
 bool InRange(int x, int y) {
-	return (0 <= x && x < n && 0 <= y && y < n);
+    return (0 <= x && x < n && 0 <= y && y < n);
 }
 
 void GrowTree() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			next_grid[i][j] = 0;
-		}
-	}
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < n; y++) {
+            next_grid[x][y] = grid[x][y];
+        }
+    }
 
-	for (int x = 0; x < n; x++) {
-		for (int y = 0; y < n; y++) {
-			if (grid[x][y] > 0) {
-				int cnt = 0;
-				for (int dir = 0; dir < DIR_NUM; dir++) {
-					int nx = x + dx[dir], ny = y + dy[dir];
-					if (InRange(nx, ny) && grid[nx][ny] > 0) cnt++;
-				}
-				next_grid[x][y] = grid[x][y] + cnt;
-			}
-			else if (grid[x][y] == -1) next_grid[x][y] = grid[x][y];
-		}
-	}
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < n; y++) {
+            if (grid[x][y] > 0) {
+                int cnt = 0;
+                for (int dir = 0; dir < DIR_NUM; dir++) {
+                    int nx = x + dx[dir], ny = y + dy[dir];
+                    if (InRange(nx, ny) && grid[nx][ny] > 0) cnt++;
+                }
+                next_grid[x][y] += cnt;
+            }
+        }
+    }
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			grid[i][j] = next_grid[i][j];
-		}
-	}
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < n; y++) {
+            grid[x][y] = next_grid[x][y];
+        }
+    }
 }
 
 void ReproduceTree() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			next_grid[i][j] = 0;
-		}
-	}
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < n; y++) {
+            next_grid[x][y] = 0;
+        }
+    }
 
-	for (int x = 0; x < n; x++) {
-		for (int y = 0; y < n; y++) {
-			if (grid[x][y] > 0) {
-				next_grid[x][y] = grid[x][y];
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < n; y++) {
+            if (grid[x][y] > 0) {
+                int cnt = 0;
+                for (int dir = 0; dir < DIR_NUM; dir++) {
+                    int nx = x + dx[dir], ny = y + dy[dir];
+                    if (InRange(nx, ny) && grid[nx][ny] == 0 && herbicide[nx][ny] == 0) cnt++;
+                }
 
-				int cnt = 0;
-				for (int dir = 0; dir < DIR_NUM; dir++) {
-					int nx = x + dx[dir], ny = y + dy[dir];
-					if (InRange(nx, ny) && grid[nx][ny] == 0 && herbicide[nx][ny] == 0) cnt++;
-				}
-				if (cnt > 0) {
-					for (int dir = 0; dir < DIR_NUM; dir++) {
-						int nx = x + dx[dir], ny = y + dy[dir];
-						if (InRange(nx, ny) && grid[nx][ny] == 0 && herbicide[nx][ny] == 0)
-							next_grid[nx][ny] += grid[x][y] / cnt;
-					}
-				}
-			}
-			else if (grid[x][y] == -1) next_grid[x][y] = grid[x][y];
-		}
-	}
+                if (cnt > 0) {
+                    int reproduce_cnt = grid[x][y] / cnt;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			grid[i][j] = next_grid[i][j];
-		}
-	}
+                    for (int dir = 0; dir < DIR_NUM; dir++) {
+                        int nx = x + dx[dir], ny = y + dy[dir];
+                        if (InRange(nx, ny) && grid[nx][ny] == 0 && herbicide[nx][ny] == 0) {
+                            next_grid[nx][ny] += reproduce_cnt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < n; y++) {
+            grid[x][y] += next_grid[x][y];
+        }
+    }
 }
 
 pair<int, int> FindMaxPos() {
-	int max_val = 0;
-	int mx = -1, my = -1;
-	for (int x = 0; x < n; x++) {
-		for (int y = 0; y < n; y++) {
-			if (grid[x][y] > 0) {
-				int val = grid[x][y];
-				for (int dir = 0; dir < DIAG_DIR_NUM; dir++) {
-					for (int i = 1; i <= k; i++) {
-						int nx = x + dx_diag[dir] * i, ny = y + dy_diag[dir] * i;
-						if (InRange(nx, ny) && grid[nx][ny] >= 0)
-							val += grid[nx][ny];
-						else break;
-					}
-				}
-				if (max_val < val) {
-					max_val = val;
-					mx = x, my = y;
-				}
-			}
-		}
-	}
-	return { mx, my };
+    int max_val = 0;
+    int mx = -1, my = -1;
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < n; y++) {
+            if (grid[x][y] > 0) {
+                int val = grid[x][y];
+                for (int dir = 0; dir < DIAG_DIR_NUM; dir++) {
+                    for (int i = 1; i <= k; i++) {
+                        int nx = x + dx_diag[dir] * i, ny = y + dy_diag[dir] * i;
+                        if (InRange(nx, ny) && grid[nx][ny] >= 0) {
+                            val += grid[nx][ny];
+                            if (grid[nx][ny] == 0 || herbicide[nx][ny] > 0) break;
+                        }
+                        else break;
+                    }
+                }
+                if (max_val < val) {
+                    max_val = val;
+                    mx = x, my = y;
+                }
+            }
+        }
+    }
+    return { mx, my };
 }
 
 void RemoveTree() {
-	int x, y;
-	tie(x, y) = FindMaxPos();
-	int val = grid[x][y];
-	herbicide[x][y] = c + 1;
-	grid[x][y] = 0;
-	for (int dir = 0; dir < DIAG_DIR_NUM; dir++) {
-		for (int i = 1; i <= k; i++) {
-			int nx = x + dx_diag[dir] * i, ny = y + dy_diag[dir] * i;
-			if (InRange(nx, ny) && grid[nx][ny] >= 0) {
-				val += grid[nx][ny];
-				grid[nx][ny] = 0;
-				herbicide[nx][ny] = c + 1;
-			}
-			else break;
-		}
-	}
+    int x, y;
+    tie(x, y) = FindMaxPos();
+    int val = grid[x][y];
+    herbicide[x][y] = c + 1;
+    grid[x][y] = 0;
+    for (int dir = 0; dir < DIAG_DIR_NUM; dir++) {
+        for (int i = 1; i <= k; i++) {
+            int nx = x + dx_diag[dir] * i, ny = y + dy_diag[dir] * i;
+            if (InRange(nx, ny) && grid[nx][ny] > 0) {
+                val += grid[nx][ny];
+                grid[nx][ny] = 0;
+                herbicide[nx][ny] = c + 1;
+            } else break;
+        }
+    }
 
-	ans += val;
+    ans += val;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (herbicide[i][j] > 0)
-				herbicide[i][j]--;
-		}
-	}
-}
-
-void Print() {
-	cout << "====================\n";
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << grid[i][j] << " ";
-		}
-		cout << "\n";
-	}
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (herbicide[i][j] > 0)
+                herbicide[i][j]--;
+        }
+    }
 }
 
 void Simulate() {
-	//Print();
-	GrowTree();
-	//Print();
-	ReproduceTree();
-	//Print();
-	RemoveTree();
-	//Print();
+    GrowTree();
+    ReproduceTree();
+    RemoveTree();
 }
 
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	//freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    //freopen("input.txt", "r", stdin);
 
-	cin >> n >> m >> k >> c;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> grid[i][j];
-		}
-	}
+    cin >> n >> m >> k >> c;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
 
-	while (m--) Simulate();
+    while (m--) Simulate();
 
-	cout << ans << "\n";
-	return 0;
+    cout << ans << "\n";
+    return 0;
 }
