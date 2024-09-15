970139545711b0ef23235156bc58a32b5cffde8e
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-09-15 15:04:22 +0900

    Title: 마법의 숲 탐색, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240915/마법의 숲 탐색/magical-forest-exploration.cpp
+++ 240915/마법의 숲 탐색/magical-forest-exploration.cpp
@@ -1,194 +1,101 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <algorithm>
 using namespace std;
-int R, C, K, ans;
 
-int dy[4] = { 1,-1,0,0 };
-int dx[4] = { 0,0,1,-1 };
+#define MAX_L 70
 
-// 0은 빈칸, 나머지는 골렘 번호
-// 낙하지점을 0으로 시작하면 세로 길이 +2 하기
-int Map[73][73];
-// 출구 확인 
-bool golem_exit[73][73];
-// d : (0,1,2,3) = (북,동,남,서)
-struct golem_info {
-	int c, d;
-};
+int R, C, K;
+int A[MAX_L + 3][MAX_L];
+int dy[4] = { -1, 0, 1, 0 }, dx[4] = { 0, 1, 0, -1 };
+bool isExit[MAX_L + 3][MAX_L];
+int ans = 0;
 
-vector<golem_info>golem;
-
-void input() {
-	cin >> R >> C >> K;
-	for (int i = 0; i < K; i++) {
-		int c, d;
-		cin >> c >> d;
-		golem.push_back({ c,d });
-	}
+bool InRange(int y, int x) {
+	return 3 <= y && y < R + 3 && 0 <= x && x < C;
 }
 
-bool isValid(int y, int x) {
-	return Map[y][x] == 0 && Map[y - 1][x] == 0 && Map[y + 1][x] == 0 && Map[y][x - 1] == 0 && Map[y][x + 1] == 0 && y <= R + 1 && x >= 2 && x <= C-1;
-}
-
-bool isValid2(int y, int x) {
-	return y <= R + 2 && x >= 1 && x <= C && Map[y][x] != 0;
-}
-
-bool isValid3(int y, int x,int num) {
-	return y <= R + 2 && x >= 1 && x <= C && Map[y][x]==num;
-}
-
-void mapRemove() {
-	for (int i = 0; i < 73; i++) {
-		for (int j = 0; j < 73; j++) {
-			Map[i][j] = 0;
-			golem_exit[i][j] = false;
+void ResetMap() {
+	for (int i = 0; i < R + 3; i++) {
+		for (int j = 0; j < C; j++) {
+			A[i][j] = 0;
+			isExit[i][j] = false;
 		}
 	}
 }
 
-void bfs(int y,int x,int num) {
-	queue<pair<int, int>>q;
-	int max_y = y;
-	// 첫 십자 좌표 체크 및 4방향 좌표 큐에 추가
-	bool visited[73][73] = { false };
+bool CanGo(int y, int x) {
+	bool flag = 0 <= x - 1 && x + 1 < C && y + 1 < R + 3;
+	flag = flag && (A[y - 1][x - 1] == 0);
+	flag = flag && (A[y - 1][x] == 0);
+	flag = flag && (A[y - 1][x+1] == 0);
+	flag = flag && (A[y][x-1] == 0);
+	flag = flag && (A[y][x] == 0);
+	flag = flag && (A[y][x+1] == 0);
+	flag = flag && (A[y+1][x] == 0);
+	return flag;
+}
+
+int BFS(int y, int x) {
+	int rslt = y;
+	queue<pair<int, int>> q;
+	bool visited[MAX_L + 3][MAX_L];
+	for (int i = 0; i < R + 3; i++)	{
+		for (int j = 0; j < C; j++) {
+			visited[i][j] = false;
+		}
+	}
+
+	q.push({ y, x });
 	visited[y][x] = true;
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		visited[ny][nx] = true;
-		q.push({ ny,nx });
-	}
-
-	// 지금 위치가 출구 또는 지금 위치가 출구가 아닐 때 2가지로 나눔
 	while (!q.empty()) {
-		pair<int, int> now = q.front();
+		pair<int, int> cur = q.front();
 		q.pop();
-		visited[now.first][now.second] = true;
-
-		// 최대 y좌표 갱신
-		max_y = max(max_y, now.first);
-		// 지금 위치가 출구일 때
-		if (golem_exit[now.first][now.second]) {
-			for (int i = 0; i < 4; i++) {
-				int ny = now.first + dy[i];
-				int nx = now.second + dx[i];
-				if (visited[ny][nx])continue;
-				// 다음 위치가 빈칸이 아니면 추가
-				if (!isValid2(ny, nx))continue;
+		for (int k = 0; k < 4; k++) {
+			int ny = cur.first + dy[k], nx = cur.second + dx[k];
+			if (InRange(ny, nx) && !visited[ny][nx] && (A[ny][nx] == A[cur.first][cur.second] || (A[ny][nx] != 0 && isExit[cur.first][cur.second]))) {
+				q.push({ ny, nx });
 				visited[ny][nx] = true;
-				q.push({ ny,nx });
-			}
-		}
-		// 지금 위치가 출구가 아닐 때
-		else if (Map[now.first][now.second] >=1) {
-			for (int i = 0; i < 4; i++) {
-				int ny = now.first + dy[i];
-				int nx = now.second + dx[i];
-				if (visited[ny][nx])continue;
-				// 다음 위치가 자신의 골렘 번호일 때만 추가
-				if (!isValid3(ny, nx,Map[now.first][now.second]))continue;
-				visited[ny][nx] = true;
-				q.push({ ny,nx });
+				rslt = max(rslt, ny);
 			}
 		}
 	}
-	// 정답 추가
-	ans += max_y - 2;
+	return rslt;
 }
 
-void moveGolem(golem_info a,int num) {
-	// R x C 맵에서 시작 중앙점은 (0,C)
-	// 실제 맵은 2행부터 있음!
-	int sR = 0;
-	int sC = a.c;
-	int sD = a.d;
-	
-	while (1) {
-		// 남쪽
-		sR++;
-		if (isValid(sR,sC)) {
-			continue;
-		}
-		sR--;
-		// 서쪽
-		sC--;
-		if (isValid(sR,sC)) {
-			sR++;
-			if (isValid(sR, sC)) {
-				if (sD == 0)sD = 3;
-				else sD--;
-				continue;
-			}
-			else {
-				sR--;
-			}
-		}
-		sC++;
-		// 동쪽
-		sC++;
-		if (isValid(sR,sC)) {
-			sR++;
-			if (isValid(sR, sC)) {
-				sD = (sD + 1) % 4;
-				continue;
-			}
-			else {
-				sR--;
-			}
-		}
-		sC--;
-		// 3가지 다 안되면 종료
-		break;
+void down(int y, int x, int d, int id) {
+	if (CanGo(y + 1, x)) {
+		down(y + 1, x, d, id);
 	}
-
-	// 이동 후 맵 밖이면 초기화
-	if (sR <= 3) {
-		mapRemove();
-		return;
+	else if (CanGo(y + 1, x - 1)) {
+		down(y + 1, x - 1, (d + 3) % 4, id);
 	}
-	// 이동 후 맵 안이면 적용
+	else if (CanGo(y + 1, x + 1)) {
+		down(y + 1, x + 1, (d + 1) % 4, id);
+	}
 	else {
-		Map[sR][sC] = num;
-		Map[sR-1][sC] = num;
-		Map[sR+1][sC] = num;
-		Map[sR][sC - 1] = num;
-		Map[sR][sC + 1] = num;
-		// 북
-		if (sD == 0) {
-			golem_exit[sR - 1][sC] = true;
+		if (!InRange(y - 1, x - 1) || !InRange(y + 1, x + 1)) {
+			ResetMap();
 		}
-		// 동
-		else if (sD == 1) {
-			golem_exit[sR][sC + 1] = true;
+		else {
+			A[y][x] = id;
+			for (int k = 0; k < 4; k++) {
+				A[y + dy[k]][x + dx[k]] = id;
+			}
+			isExit[y + dy[d]][x + dx[d]] = true;
+			ans += BFS(y, x) - 3 + 1;
 		}
-		// 남
-		else if (sD == 2) {
-			golem_exit[sR + 1][sC] = true;
-		}
-		// 서
-		else if (sD == 3) {
-			golem_exit[sR][sC - 1] = true;
-		}
-
-		// 도착 후 맵에서 BFS를 통한 최종 목적지 구하기
-		bfs(sR,sC,num);
 	}
-
-}
-
-void func() {
-	for (int i = 0; i < K; i++) {
-		moveGolem(golem[i],i+1);
-	}
-	cout << ans;
 }
 
 int main() {
-	input();
-	func();
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> R >> C >> K;
+	for (int id = 1; id <= K; id++) {
+		int x, d;
+		cin >> x >> d;
+		x--;
+		down(0, x, d, id);
+	}
+	cout << ans << "\n";
 	return 0;
 }
