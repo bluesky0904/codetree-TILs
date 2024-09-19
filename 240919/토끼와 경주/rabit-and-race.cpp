40fa13642e34335b9afc63b8f153beae0f7972c0
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-09-19 15:28:15 +0900

    Title: 토끼와 경주, Time: 1081ms, Memory: 0MB, Status: Passed - CodeTree

--- 240919/토끼와 경주/rabit-and-race.cpp
+++ 240919/토끼와 경주/rabit-and-race.cpp
@@ -1,207 +1,277 @@
-#include <bits/stdc++.h>
-#define endl '\n'
-#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+/*
+토끼 : P마리. 고유한 번호, 한번 움직일 시 꼭 이동해야 하는 거리
+처음 토끼들은 전부 (1행, 1열)에 있음
+격자 : N x M
+(1) 경주 시작 준비
+(2) 경주 진행
+가장 우선순위가 높은 토끼를 뽑아 멀리 보내주는 것을 K번 반복
+현재까지의 총 점프 횟수 최소 > 행 번호 + 열 번호 최소 > 행 번호 최소 > 열 번호 최소 > 고유번호 최소
+우선순위가 가장 높은 토끼 : 상하좌우 di만큼 이동했을 때 행 + 열 번호 최대 > 행 번호 최대 > 열 번호 최대
+i번 토끼를 제외한 나머지 P-1마리의 토끼들은 전부 ri + ci만큼의 점수를 동시에 얻게 됨.
+K번의 턴이 모두 진행된 이후 행 + 열 번호 최대 > 행 최대 > 열 최대 > 고유번호 최대 토끼에게 S점 +
+단, K번의 턴 동안 한번이라도 뽑혔던 적이 있던 토끼 중 가장 우선순위가 높은 토끼를 골라야 함.
+(3) 이동거리 변경
+고유번호가 pidt인 토끼의 이동거리를 L배 해줌.
+(4) 최고의 토끼 선정
+각 토끼가 모든 경주를 진행하며 얻은 점수 중 가장 높은 점수를 출력
+*/
+
+#include <iostream>
+#include <queue>
+#include <map>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll, ll> pll;
-typedef tuple<int,int,int> tiii;
-const int INF=1e9+1;
+#define MAX_N 2000
 
-struct Rabbit {
-    int x, y;
-    int pid;
-    int d;
-    int jump_count;
-    // 토끼 우선순위
-    bool operator<(const Rabbit& cmp) const {
-        if(jump_count==cmp.jump_count) {
-            if(x+y==cmp.x+cmp.y) {
-                if(x==cmp.x) {
-                    if(y==cmp.y) return pid>cmp.pid;
-                    return y>cmp.y;
-                }
-                return x>cmp.x;
-            }
-            return x+y>cmp.x+cmp.y;
-        }
-        return jump_count>cmp.jump_count;
-    }
-};
-
-bool cmp(pii& a, pii& b) {
-    // 토끼 이동 정렬
-    if(a.first+a.second==b.first+b.second) {
-        if(a.first==a.second) return a.second>b.second;
-        return a.first>b.first;
-    }
-    return a.first+a.second>b.first+b.second;
-}
-
-bool cmp2(Rabbit& a, Rabbit& b) {
-    // 점수를 추가할 토끼 정렬
-    if(a.x+a.y==b.x+b.y) {
-        if(a.x==b.x) {
-            if(a.y==b.y) return a.pid>b.pid;
-            return a.y>b.y;
-        }
-        return a.x>b.x;
-    }
-    return a.x+a.y>b.x+b.y;
-}
-
-unordered_map<int,Rabbit> rabbits; // (토끼 id, 토끼)
-unordered_map<int,ll> score; // (토끼 id, 점수)
-priority_queue<Rabbit> pq;
 int n, m, p;
 
-void init(int p) {
-    cin>>n>>m>>p;
-    for(int i=0;i<p;i++) {
-        int pid, d; cin>>pid>>d;
-        rabbits[pid]={0,0,pid,d,0};
-    }
-    for(auto it : rabbits) {
-        pq.push(it.second);
-        score[it.first]=0;
-    }
+// 각 토끼의 id
+int id[MAX_N + 1];
+// 각 토끼의 이동거리
+int pw[MAX_N + 1];
+// 각 토끼의 점프 횟수
+int jump_cnt[MAX_N + 1];
+// 각 토끼의 점수
+long long result[MAX_N + 1];
+// 각 토끼의 현재 위치
+pair<int, int> point[MAX_N + 1];
+// 각 토끼의 id를 인덱스 번호로 변환
+map<int, int> id_to_idx;
+// 각각의 경주에서 토끼가 달렸는지 여부
+bool is_runned[MAX_N + 1];
+
+long long total_sum;
+
+struct Rabbit {
+	int x, y, j, id;
+
+	// 이동할 토끼를 결정하기 위해 정렬함수를 만들어 줌
+	bool operator < (const Rabbit& b) const {
+		if (j != b.j) return j > b.j;
+		if (x + y != b.x + b.y) return x + y > b.x + b.y;
+		if (x != b.x) return x > b.x;
+		if (y != b.y) return y > b.y;
+		return id > b.id;
+	}
+};
+
+// 가장 긴 위치를 판단하기 위함의 정렬함수
+bool Cmp(Rabbit a, Rabbit b) {
+	if (a.x + a.y != b.x + b.y) return a.x + a.y < b.x + b.y;
+	if (a.x != b.x) return a.x < b.x;
+	if (a.y != b.y) return a.y < b.y;
+	return a.id < b.id;
 }
 
-// 토끼 이동은 반복되므로 나머지 연산을 적용한다
-// distance %= 2 * ((N or M) -1)
-// 한 방향으로 이동, 반대로 이동, 다시 반대로 이동이 하나의 사이클이 된다
-
-pii right(int x, int y, int d) {
-    int dist=d%(2*(m-1));
-    if(m-y-1<dist) {
-        dist-=m-y-1;
-        if(m-1<dist) {
-            dist-=m-1;
-            y=dist;
-        }
-        else y=m-1-dist;
-    }
-    else y+=dist;
-    return {x,y};
+// 경주 시작 준비 쿼리를 처리
+void init() {
+	cin >> n >> m >> p;
+	for (int i = 1; i <= p; i++) {
+		cin >> id[i] >> pw[i];
+		id_to_idx[id[i]] = i;
+		point[i] = make_pair(1, 1);
+	}
 }
 
-pii left(int x, int y, int d) {
-    int dist=d%(2*(m-1));
-    if(y<dist) {
-        dist-=y;
-        if(m-1<dist) {
-            dist-=m-1;
-            y=m-1-dist;
-        }
-        else y=dist;
-    }
-    else y-=dist;
-    return {x,y};
+Rabbit GetUpRabbit(Rabbit cur_rabbit, int dis) {
+	Rabbit up_rabbit = cur_rabbit;
+	dis %= 2 * (n - 1);
+
+	if (dis >= up_rabbit.x - 1) {
+		dis -= (up_rabbit.x - 1);
+		up_rabbit.x = 1;
+	}
+	else {
+		up_rabbit.x -= dis;
+		dis = 0;
+	}
+
+	if (dis >= n - up_rabbit.x) {
+		dis -= (n - up_rabbit.x);
+		up_rabbit.x = n;
+	}
+	else {
+		up_rabbit.x += dis;
+		dis = 0;
+	}
+
+	up_rabbit.x -= dis;
+
+	return up_rabbit;
 }
 
-pii up(int x, int y, int d) {
-    int dist=d%(2*(n-1));
-    if(x<dist) {
-        dist-=x;
-        if(n-1<dist) {
-            dist-=n-1;
-            x=n-1-dist;
-        }
-        else x=dist;
-    }
-    else x-=dist;
-    return {x,y};
+Rabbit GetDownRabbit(Rabbit cur_rabbit, int dis) {
+	Rabbit down_rabbit = cur_rabbit;
+	dis %= 2 * (n - 1);
+
+	if (dis >= n - down_rabbit.x) {
+		dis -= (n - down_rabbit.x);
+		down_rabbit.x = n;
+	}
+	else {
+		down_rabbit.x += dis;
+		dis = 0;
+	}
+
+	if (dis >= down_rabbit.x - 1) {
+		dis -= (down_rabbit.x - 1);
+		down_rabbit.x = 1;
+	}
+	else {
+		down_rabbit.x -= dis;
+		dis = 0;
+	}
+
+	down_rabbit.x += dis;
+
+	return down_rabbit;
 }
 
-pii down(int x, int y, int d) {
-    int dist=d%(2*(n-1));
-    if(n-x-1<dist) {
-        dist-=n-x-1;
-        if(n-1<dist) {
-            dist-=n-1;
-            x=dist;
-        }
-        else x=n-1-dist;
-    }
-    else x+=dist;
-    return {x,y};
+Rabbit GetLeftRabbit(Rabbit cur_rabbit, int dis) {
+	Rabbit left_rabbit = cur_rabbit;
+	dis %= 2 * (m - 1);
+
+	if (dis >= left_rabbit.y - 1) {
+		dis -= (left_rabbit.y - 1);
+		left_rabbit.y = 1;
+	}
+	else {
+		left_rabbit.y -= dis;
+		dis = 0;
+	}
+
+	if (dis >= m - left_rabbit.y) {
+		dis -= (m - left_rabbit.y);
+		left_rabbit.y = m;
+	}
+	else {
+		left_rabbit.y += dis;
+		dis = 0;
+	}
+
+	left_rabbit.y -= dis;
+
+	return left_rabbit;
 }
 
-void race(int k, int s) { // 토끼 이동
-    set<int> selected;
-    while(k--) {
-        auto [x,y,pid,dist,jump_count]=pq.top();
-        pq.pop();
-        selected.insert(pid);
+Rabbit GetRightRabbit(Rabbit cur_rabbit, int dis) {
+	Rabbit right_rabbit = cur_rabbit;
+	dis %= 2 * (m - 1);
 
-        vector<pii> v;
-        pii r=right(x,y,dist);
-        v.push_back(r);
-        pii l=left(x,y,dist);
-        v.push_back(l);
-        pii u=up(x,y,dist);
-        v.push_back(u);
-        pii d=down(x,y,dist);
-        v.push_back(d);
-        
-        sort(v.begin(),v.end(),cmp);
-        x=v.front().first, y=v.front().second;
+	if (dis >= m - right_rabbit.y) {
+		dis -= (m - right_rabbit.y);
+		right_rabbit.y = m;
+	}
+	else {
+		right_rabbit.y += dis;
+		dis = 0;
+	}
 
-        rabbits[pid].x=x, rabbits[pid].y=y, rabbits[pid].jump_count++;
-        pq.push(rabbits[pid]);
-        
-        // score
-        for(auto& it : score) {
-            if(it.first!=pid) it.second+=x+y+2;
-        }
-    }
-    vector<Rabbit> v;
-    for(auto pid : selected) v.push_back(rabbits[pid]);
-    sort(v.begin(),v.end(),cmp2);
-    score[v.front().pid]+=s;
+	if (dis >= right_rabbit.y - 1) {
+		dis -= (right_rabbit.y - 1);
+		right_rabbit.y = 1;
+	}
+	else {
+		right_rabbit.y -= dis;
+		dis = 0;
+	}
+
+	right_rabbit.y += dis;
+
+	return right_rabbit;
 }
 
-void multiply(int pid, int l) { // 이동거리 곱
-    rabbits[pid].d*=l;
-    while(!pq.empty()) pq.pop();
-    for(auto it : rabbits) pq.push(it.second);
+
+void start_round() {
+	int k, bonus;
+	cin >> k >> bonus;
+	priority_queue<Rabbit> rabbit_pq;
+
+	for (int i = 1; i <= p; i++) {
+		is_runned[i] = false;
+	}
+
+	for (int i = 1; i <= p; i++) {
+		Rabbit new_rabbit;
+		new_rabbit.x = point[i].first;
+		new_rabbit.y = point[i].second;
+		new_rabbit.j = jump_cnt[i];
+		new_rabbit.id = id[i];
+
+		rabbit_pq.push(new_rabbit);
+	}
+
+	while (k--) {
+		Rabbit cur_rabbit = rabbit_pq.top(); rabbit_pq.pop();
+
+		int dis = pw[id_to_idx[cur_rabbit.id]];
+		Rabbit nex_rabbit = cur_rabbit;
+		nex_rabbit.x = 0;
+		nex_rabbit.y = 0;
+
+		Rabbit up_rabbit = GetUpRabbit(cur_rabbit, dis);
+		if (Cmp(nex_rabbit, up_rabbit)) nex_rabbit = up_rabbit;
+
+		Rabbit down_rabbit = GetDownRabbit(cur_rabbit, dis);
+		if (Cmp(nex_rabbit, down_rabbit)) nex_rabbit = down_rabbit;
+
+		Rabbit left_rabbit = GetLeftRabbit(cur_rabbit, dis);
+		if (Cmp(nex_rabbit, left_rabbit)) nex_rabbit = left_rabbit;
+
+		Rabbit right_rabbit = GetRightRabbit(cur_rabbit, dis);
+		if (Cmp(nex_rabbit, right_rabbit)) nex_rabbit = right_rabbit;
+
+		nex_rabbit.j++;
+		rabbit_pq.push(nex_rabbit);
+
+		int nex_idx = id_to_idx[nex_rabbit.id];
+		point[nex_idx] = make_pair(nex_rabbit.x, nex_rabbit.y);
+		jump_cnt[nex_idx]++;
+
+		is_runned[nex_idx] = true;
+
+		result[nex_idx] -= (nex_rabbit.x + nex_rabbit.y);
+		total_sum += (nex_rabbit.x + nex_rabbit.y);
+	}
+
+	Rabbit bonus_rabbit;
+	bonus_rabbit.x = 0;
+	bonus_rabbit.y = 0;
+	while (!rabbit_pq.empty()) {
+		Rabbit cur_rabbit = rabbit_pq.top(); rabbit_pq.pop();
+
+		if (!is_runned[id_to_idx[cur_rabbit.id]]) continue;
+
+		if (Cmp(bonus_rabbit, cur_rabbit)) bonus_rabbit = cur_rabbit;
+	}
+
+	result[id_to_idx[bonus_rabbit.id]] += bonus;
 }
 
-ll bestRabbit() { // 최고의 토끼를 선정, 점수는 int 범위를 넘어간다
-    ll max_score=0;
-    for(auto it : score) max_score=max(max_score, it.second);
-    return max_score;
+void power_up(){
+	int id, t;
+	cin >> id >> t;
+	int idx = id_to_idx[id];
+
+	pw[idx] *= t;
+}
+
+void print_result() {
+	long long ans = 0;
+	for (int i = 1; i <= p; i++)
+		ans = max(ans, result[i] + total_sum);
+	cout << ans;
 }
 
 int main() {
-    fastio
-    //freopen("input.txt","r",stdin);
-    int query_cnt; cin>>query_cnt;
-    while(query_cnt--) {
-        int cmd; cin>>cmd;
-        switch(cmd) {
-            case 100: {
-                init(p);
-                break;
-            }
-            case 200: {
-                int k,s; cin>>k>>s;
-                race(k,s);
-                break;
-            }
-            case 300: {
-                int pid, l; cin>>pid>>l;
-                multiply(pid,l);
-                break;
-            }
-            case 400: {
-                cout<<bestRabbit()<<endl;
-                break;
-            }
-            default:
-                break;
-        }
-    }
-    return 0;
+	int q;
+	cin >> q;
+	while (q--) {
+		int query;
+		cin >> query;
+		if (query == 100) init();
+		if (query == 200) start_round();
+		if (query == 300) power_up();
+		if (query == 400) print_result();
+	}
 }
