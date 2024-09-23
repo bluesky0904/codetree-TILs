7c6105b7f2bdb34019f826ade59bc95f81ede92f
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-09-23 22:30:46 +0900

    Title: 코드트리 빵, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/코드트리 빵/codetree-mon-bread.cpp
+++ 240923/코드트리 빵/codetree-mon-bread.cpp
@@ -24,8 +24,6 @@ t번 사람이 편의점을 향해 움직이기 시작했더라도 해당 베이
 
 #include <iostream>
 #include <queue>
-#include <vector>
-#include <algorithm>
 #include <tuple>
 #include <climits>
 using namespace std;
@@ -34,15 +32,15 @@ using namespace std;
 #define MAX_M 30
 #define DIR_NUM 4
 
-int n, m;
-pair<int, int> store[MAX_M + 1];
+int n, m, t = 0;
 pair<int, int> pos[MAX_M + 1];
+pair<int, int> store[MAX_M + 1];
 
-int grid[MAX_N+1][MAX_N+1]; // 0의 경우에는 빈 공간, 1의 경우에는 베이스캠프
-bool is_passed[MAX_N + 1][MAX_N + 1] = { false };
+int grid[MAX_N + 1][MAX_N + 1]; // 0의 경우 빈 공간, 1의 경우 베이스캠프
+bool NonPass[MAX_N + 1][MAX_N + 1];
 
-bool visited[MAX_N + 1][MAX_N + 1];
 int dist[MAX_N + 1][MAX_N + 1];
+bool visited[MAX_N + 1][MAX_N + 1];
 
 int dx[DIR_NUM] = { -1,0,0,1 };
 int dy[DIR_NUM] = { 0,-1,1,0 };
@@ -63,12 +61,11 @@ void BFS(pair<int, int> start_pos) {
 	q.push(start_pos);
 	visited[start_pos.first][start_pos.second] = true;
 	dist[start_pos.first][start_pos.second] = 0;
-	
 	while (!q.empty()) {
 		int cx, cy; tie(cx, cy) = q.front(); q.pop();
 		for (int dir = 0; dir < DIR_NUM; dir++) {
 			int nx = cx + dx[dir], ny = cy + dy[dir];
-			if (InRange(nx, ny) && !visited[nx][ny] && !is_passed[nx][ny]) {
+			if (InRange(nx, ny) && !visited[nx][ny] && !NonPass[nx][ny]) {
 				q.push(make_pair(nx, ny));
 				visited[nx][ny] = true;
 				dist[nx][ny] = dist[cx][cy] + 1;
@@ -79,9 +76,7 @@ void BFS(pair<int, int> start_pos) {
 
 pair<int, int> GetNextPos(int x, int y, int idx) {
 	BFS(store[idx]);
-
-	int mx = -1, my = -1;
-	int min_dist = INT_MAX;
+	int mx = -1, my = -1, min_dist = INT_MAX;
 	for (int dir = 0; dir < DIR_NUM; dir++) {
 		int nx = x + dx[dir], ny = y + dy[dir];
 		if (InRange(nx, ny) && visited[nx][ny] && min_dist > dist[nx][ny]) {
@@ -96,21 +91,18 @@ pair<int, int> GetNextPos(int x, int y, int idx) {
 void GoStore() {
 	for (int i = 1; i <= m; i++) {
 		if (pos[i] == make_pair(-1, -1) || pos[i] == store[i]) continue;
-		int x, y;
-		tie(x, y) = pos[i];
+		int x, y; tie(x, y) = pos[i];
 		pos[i] = GetNextPos(x, y, i);
 	}
 
 	for (int i = 1; i <= m; i++) {
-		if (pos[i] == store[i]) is_passed[pos[i].first][pos[i].second] = true;
+		if (pos[i] == store[i]) NonPass[pos[i].first][pos[i].second] = true;
 	}
 }
 
 void GoBase(int idx) {
 	BFS(store[idx]);
-
-	int mx = -1, my = -1;
-	int min_dist = INT_MAX;
+	int mx = -1, my = -1, min_dist = INT_MAX;
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			if (grid[i][j] == 1 && visited[i][j] && min_dist > dist[i][j]) {
@@ -120,39 +112,36 @@ void GoBase(int idx) {
 			}
 		}
 	}
+
+	NonPass[mx][my] = true;
 	pos[idx] = make_pair(mx, my);
-	is_passed[mx][my] = true;
 }
 
 bool Finish() {
-	for (int i = 1; i <= m; i++) if (pos[i] != store[i]) return false;
+	for (int i = 1; i <= m; i++) {
+		if (pos[i] != store[i]) return false;
+	}
 	return true;
 }
-
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 	cin >> n >> m;
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			cin >> grid[i][j];
 		}
 	}
-
 	for (int i = 1; i <= m; i++) {
 		int x, y; cin >> x >> y;
 		store[i] = make_pair(x, y);
 		pos[i] = make_pair(-1, -1);
 	}
 
-	int t = 0;
 	while (1) {
 		t++;
 		GoStore();
 		if (t <= m) GoBase(t);
 		if (Finish()) break;
 	}
-
 	cout << t << "\n";
 	return 0;
 }
