d7bea217c876fd39414c33c8da40e89b4de146b5
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-11-19 21:59:29 +0900

    Title: 앞에서부터 삭제하기 2, Time: 32ms, Memory: 1MB, Status: Passed - CodeTree

--- 241119/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
+++ 241119/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -1,39 +1,33 @@
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <set>
 #include <iomanip>
+#include <functional>
 using namespace std;
 
+#define MAX_N 100000
+
+int N;
+int arr[MAX_N];
+int sum = 0;
+double max_avg;
+priority_queue<int, vector<int>, greater<>> pq;
+
 int main() {
-    int N;
-    cin >> N;
-    vector<int> v(N);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
 
-    for (int i = 0; i < N; i++) {
-        cin >> v[i];
-    }
+	sum += arr[N - 1];
+	pq.push(arr[N - 1]);
+	for (int i = N - 2; i >= 1; i--) {
+		pq.push(arr[i]);
+		sum += arr[i];
+		double avg = double(sum - pq.top()) / (N - i - 1);
+		if (max_avg < avg) max_avg = avg;
+	}
 
-    double max_avg = -1e9;  // 최대 평균값을 저장할 변수
-
-    for (int K = 1; K <= N - 2; K++) {
-        multiset<int> ms;  // 정렬된 상태를 유지하는 멀티셋
-        double sum = 0;
-
-        // K개의 요소를 제외한 나머지 요소들을 멀티셋에 추가
-        for (int i = K; i < N; i++) {
-            ms.insert(v[i]);
-            sum += v[i];
-        }
-
-        // 가장 작은 값을 하나 제거
-        sum -= *ms.begin();
-        ms.erase(ms.begin());
-
-        int count = N - K - 1; // 남은 요소 수
-        double avg = sum / count;
-        max_avg = max(max_avg, avg);
-    }
-
-    cout << fixed << setprecision(2) << max_avg << "\n";
-    return 0;
+	cout << fixed << setprecision(2) << max_avg << "\n";
+	return 0;
 }
