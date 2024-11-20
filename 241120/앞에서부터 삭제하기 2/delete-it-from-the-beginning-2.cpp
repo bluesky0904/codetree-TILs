b619264c8ff23c1deb6d80a17f6f8b57b507f21b
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-11-20 13:29:54 +0900

    Title: 앞에서부터 삭제하기 2, Time: 35ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 241120/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <functional>
+#include <iomanip>
+using namespace std;
+
+#define MAX_N 100000
+
+int N;
+int arr[MAX_N];
+int sum = 0;
+double max_avg;
+priority_queue<int, vector<int>, greater<>> pq;
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	sum += arr[N - 1];
+	pq.push(arr[N - 1]);
+	for (int i = N - 2; i >= 1; i--) {
+		sum += arr[i];
+		pq.push(arr[i]);
+		double avg = double(sum - pq.top()) / (N - i - 1);
+		if (max_avg < avg) max_avg = avg;
+	}
+
+	cout << fixed << setprecision(2) << max_avg << "\n";
+	return 0;
+}
