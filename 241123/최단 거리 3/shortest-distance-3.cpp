d88cb9ce539584df99e32e9888b4c6f7ef28f5c0
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-11-23 16:25:04 +0900

    Title: 최단 거리 3, Time: 34ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 241123/최단 거리 3/shortest-distance-3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+#define MAX_N 1000
+
+vector<pair<int, int>> graph[MAX_N + 1];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+int dist[MAX_N + 1];
+
+int n, m, A, B;
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= m; i++) {
+		int v1, v2, d;
+		cin >> v1 >> v2 >> d;
+		graph[v1].push_back({ v2, d });
+		graph[v2].push_back({ v1, d });
+	}
+
+	cin >> A >> B;
+
+	fill(dist, dist + MAX_N + 1, (int)1e9);
+
+	dist[A] = 0;
+	pq.push({ 0, A });
+	while (!pq.empty()) {
+		int min_dist, min_index;
+		tie(min_dist, min_index) = pq.top();
+		pq.pop();
+
+		if (min_dist != dist[min_index]) continue;
+
+		for (int i = 0; i < (int)graph[min_index].size(); i++) {
+			int target_index, target_dist;
+			tie(target_index, target_dist) = graph[min_index][i];
+
+			int new_dist = min_dist + target_dist;
+			if (dist[target_index] > new_dist) {
+				dist[target_index] = new_dist;
+				pq.push({ new_dist, target_index });
+			}
+		}
+	}
+
+	cout << dist[B] << "\n";
+	return 0;
+}
