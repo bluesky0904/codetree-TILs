c3cd2fdc88d6c90264afcfd40e12ec00441156b9
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-11-24 21:29:57 +0900

    Title: 천 개의 정거장, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241124/천 개의 정거장/thousand-stops.cpp
+++ 241124/천 개의 정거장/thousand-stops.cpp
@@ -7,26 +7,31 @@
 using namespace std;
 
 #define MAX_N 1000
-#define INF (int)1e9
+#define INF (long long)1e18
 
-// {다음 지점, 버스 번호, 탑승료}
-vector<tuple<int, int, int>> graph[MAX_N + 1];
+// {다음 지점, 버스 번호, 탑승료, 현재 순서}
+struct Edge {
+    int next_index;
+    int bus_number;
+    long long fee;
+    int order;
+};
 
-// {현재 비용, 현재 시간, 현재 버스 번호, 현재 위치}
-priority_queue<tuple<int, int, int, int>, vector<tuple<int, int, int, int>>, greater<>> pq;
+vector<Edge> graph[MAX_N + 1]; // 그래프 정의
+priority_queue<tuple<long long, long long, int, int, int>, vector<tuple<long long, long long, int, int, int>>, greater<>> pq;
+// {현재 비용, 현재 시간, 현재 버스 번호, 현재 지점, 노선 순서}
 
-// 최소 비용 및 시간 기록
-int dist[MAX_N + 1];
-int time_dist[MAX_N + 1];
+pair<long long, long long> dist[MAX_N + 1]; // {최소 비용, 최소 시간}
 
 int A, B, N;
 
 int main() {
     cin >> A >> B >> N;
 
-    // 그래프 구성
+    // 그래프 입력
     for (int i = 1; i <= N; i++) {
-        int fee, cnt;
+        long long fee;
+        int cnt;
         cin >> fee >> cnt;
 
         vector<int> stops(cnt);
@@ -34,58 +39,62 @@ int main() {
             cin >> stops[j];
         }
 
-        // 노선 순서대로 간선 추가
         for (int j = 0; j < cnt - 1; j++) {
-            graph[stops[j]].emplace_back(stops[j + 1], i, fee);
+            graph[stops[j]].push_back({stops[j + 1], i, fee, j + 1}); // 순서대로 간선 연결
         }
     }
 
-    // 거리 배열 초기화
-    fill(dist, dist + MAX_N + 1, INF);
-    fill(time_dist, time_dist + MAX_N + 1, INF);
+    // 거리 초기화
+    for (int i = 1; i <= MAX_N; i++) {
+        dist[i] = {INF, INF};
+    }
 
-    // 시작점 초기화
-    dist[A] = 0;
-    time_dist[A] = 0;
-    pq.push({0, 0, 0, A}); // {비용, 시간, 버스 번호, 현재 지점}
+    dist[A] = {0, 0};
+    pq.push({0, 0, 0, A, 0}); // {비용, 시간, 버스 번호, 현재 지점, 순서}
 
     // 다익스트라 알고리즘
     while (!pq.empty()) {
-        int cur_fee, cur_time, cur_bus, cur_index;
-        tie(cur_fee, cur_time, cur_bus, cur_index) = pq.top();
+        long long cur_fee, cur_time;
+        int cur_bus, cur_index, cur_order;
+        tie(cur_fee, cur_time, cur_bus, cur_index, cur_order) = pq.top();
         pq.pop();
 
-        // 이미 더 짧은 경로가 있으면 스킵
-        if (dist[cur_index] < cur_fee || (dist[cur_index] == cur_fee && time_dist[cur_index] < cur_time))
+        if (dist[cur_index] < make_pair(cur_fee, cur_time)) {
             continue;
+        }
 
-        // 현재 지점에서 갈 수 있는 모든 간선을 확인
         for (const auto& edge : graph[cur_index]) {
-            int next_index, next_bus, next_fee;
-            tie(next_index, next_bus, next_fee) = edge;
+            int next_index = edge.next_index;
+            int next_bus = edge.bus_number;
+            long long next_fee = edge.fee;
+            int next_order = edge.order;
 
-            int new_fee = cur_fee;
-            int new_time = cur_time + 1;
+            long long new_fee = cur_fee;
+            long long new_time = cur_time + 1;
 
-            // 다른 버스로 환승 시 탑승료 추가
+            // 다른 버스로 환승할 경우 비용 추가
             if (cur_bus != next_bus) {
                 new_fee += next_fee;
             }
 
-            // 최소 비용 및 시간 갱신
-            if (dist[next_index] > new_fee || (dist[next_index] == new_fee && time_dist[next_index] > new_time)) {
-                dist[next_index] = new_fee;
-                time_dist[next_index] = new_time;
-                pq.push({new_fee, new_time, next_bus, next_index});
+            // 마지막 지점에서 내려야 함
+            if (next_order == 1) { // 노선 시작점에서만 환승 가능
+                new_fee = cur_fee + next_fee;
+            }
+
+            // 최소 비용과 시간 갱신
+            if (dist[next_index] > make_pair(new_fee, new_time)) {
+                dist[next_index] = {new_fee, new_time};
+                pq.push({new_fee, new_time, next_bus, next_index, next_order});
             }
         }
     }
 
     // 결과 출력
-    if (dist[B] == INF) {
-        cout << "-1 -1\n"; // 도달 불가능
+    if (dist[B].first == INF) {
+        cout << "-1 -1\n";
     } else {
-        cout << dist[B] << " " << time_dist[B] << "\n"; // 최소 비용과 시간 출력
+        cout << dist[B].first << " " << dist[B].second << "\n";
     }
 
     return 0;
