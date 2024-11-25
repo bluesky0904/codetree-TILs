bbada44a92e0e8355bb30bc6cc356785f937b144
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-11-25 11:04:01 +0900

    Title: 천 개의 정거장, Time: 101ms, Memory: 16MB, Status: Passed - CodeTree

--- 241125/천 개의 정거장/thousand-stops.cpp
+++ 241125/천 개의 정거장/thousand-stops.cpp
@@ -1,98 +1,96 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <functional>
-#include <algorithm>
+#include <tuple>
+
+#define MAX_M 1000
+#define INF (long long)1e17 + 1
+
 using namespace std;
 
-#define MAX_N 1000
-#define INF (long long)1e18
+// 변수 선언
+int a, b, n, m = 1000;
+pair<long long, long long> graph[MAX_M + 1][MAX_M + 1]; // (비용, 시간)을 기록
+bool visited[MAX_M + 1];
 
-// {다음 노드, 버스 번호, 탑승 순서, 탑승료}
-typedef struct {
-	int next_idx, bus_num, order;
-	long long fee;
-}Edge;
-
-// pair<최소 비용, 최소 시간> dist[노드번호][버스번호]
-pair<long long, int> dist[MAX_N + 1][MAX_N + 1];
-vector<Edge> graph[MAX_N + 1];
-// 우선 순위 : {비용, 시간, 노드 번호, 버스 번호, 탑승 순서}
-priority_queue<tuple<long long, int, int, int, int>, vector<tuple<long long, int, int, int, int>>, greater<>> pq;
-
-int A, B, N;
+pair<long long, long long> dist[MAX_M + 1];             // (비용, 시간)을 기록
 
 int main() {
-	cin >> A >> B >> N;
+    // 입력
+    cin >> a >> b >> n;
 
-	// 그래프 받기
-	for (int i = 1; i <= N; i++) {
-		long long fee;
-		int cnt;
-		cin >> fee >> cnt;
-		vector<int> vertices;
-		for (int j = 0; j < cnt; j++) {
-			int x; cin >> x;
-			vertices.push_back(x);
-		}
+    // 초기 graph 값에 전부 큰 값 기록
+    for(int i = 1; i <= m; i++) {
+        for(int j = 1; j <= m; j++) {
+            graph[i][j] = make_pair(INF, 0);
+        }
+        // 자기 자신은 비용과 시간이 전혀 소요되지 않음
+        graph[i][i] = make_pair(0, 0);
+    }
 
-		for (int j = 0; j < cnt - 1; j++) {
-			graph[vertices[j]].push_back({ vertices[j + 1], i, j + 1, fee });
-		}
-	}
+    // 그래프를 인접행렬로 표현
+    for(int i = 1; i <= n; i++) {
+        int cost, stop_num;
+        cin >> cost >> stop_num;
+        
+        vector<int> stops(stop_num);
+        for(int j = 0; j < stop_num; j++)
+            cin >> stops[j];
 
-	// 기본값 설정
-	for (int i = 1; i <= MAX_N; i++) {
-		for (int j = 1; j <= N; j++) {
-			dist[i][j] = { INF, INF };
-		}
-	} 
+        for(int j = 0; j < stop_num; j++) {
+            for(int k = j + 1; k < stop_num; k++) {
+                graph[stops[j]][stops[k]] = min(graph[stops[j]][stops[k]],
+                                                make_pair((long long)cost, (long long)k - j));
+            }
+        }
+    }
+    
+    // 그래프에 있는 모든 노드들에 대해
+    // 초기값을 전부 아주 큰 값으로 설정
+    // INT_MAX 그 자체로 설정하면
+    // 후에 덧셈 진행시 overflow가 발생할 수도 있으므로
+    // 적당히 큰 숫자로 적어줘야함에 유의!
+    for(int i = 1; i <= m; i++)
+        dist[i] = make_pair(INF, 0);
 
-	for (int i = 1; i <= N; i++) {
-		dist[A][i] = { 0, 0 };
-	}
-	pq.push({ 0, 0, A, 0, 0 });
+    // 시작위치에는 dist값을 0으로 설정
+    dist[a] = make_pair(0, 0);
 
-	// 다익스트라 알고리즘
-	// pair<최소 비용, 최소 시간> dist[노드번호][버스번호]
-	// 간선 : {다음 노드, 버스 번호, 탑승 순서, 탑승료}
-	// 우선 순위 : {비용, 시간, 노드 번호, 버스 번호, 탑승 순서}
-	while (!pq.empty()) {
-		long long cur_fee;
-		int cur_time, cur_idx, cur_bus, cur_order;
-		tie(cur_fee, cur_time, cur_idx, cur_bus, cur_order) = pq.top();
-		pq.pop();
+    // O(|V|^2) 다익스트라 코드
+    for(int i = 1; i <= m; i++) {
+        // V개의 정점 중 
+        // 아직 방문하지 않은 정점 중
+        // dist값이 가장 작은 정점을 찾아줍니다.
+        int min_index = -1;
+        for(int j = 1; j <= m; j++) {
+            if(visited[j])
+                continue;
+            
+            if(min_index == -1 || dist[min_index] > dist[j])
+                min_index = j;
+        }
 
-		// if (dist[cur_num][cur_bus] != make_pair(cur_fee, cur_time)) continue;
+        // 최솟값에 해당하는 정점에 방문 표시를 진행합니다.
+        visited[min_index] = true;
 
-		for (int i = 0; i < (int)graph[cur_idx].size(); i++) {
-			int next_idx, next_bus, next_order;
-			long long next_fee;
-			next_idx = graph[cur_idx][i].next_idx;
-			next_bus = graph[cur_idx][i].bus_num;
-			next_order = graph[cur_idx][i].order;
-			next_fee = graph[cur_idx][i].fee;
+        long long min_cost, min_time;
+        tie(min_cost, min_time) = dist[min_index];
 
-			long long new_fee = cur_fee;
-			int new_time = cur_time + 1;
+        // 최솟값에 해당하는 정점에 연결된 간선들을 보며
+        // 시작점으로부터의 최단거리 값을 갱신해줍니다.
+        for(int j = 1; j <= m; j++) {
+            long long cost, time;
+            tie(cost, time) = graph[min_index][j];
+            dist[j] = min(dist[j], make_pair(min_cost + cost, min_time + time));
+        }
+    }
 
-			if (cur_bus != next_bus) {
-				new_fee += next_fee;
-			}
+    // 만약 도달이 불가능하다면 -1 -1이 답이 됩니다.
+    if(dist[b] == make_pair(INF, 0LL))
+        dist[b] = make_pair(-1, -1);
+    
+    long long min_cost, min_time;
+    tie(min_cost, min_time) = dist[b];
 
-			if (dist[next_idx][next_bus] > make_pair(new_fee, new_time)) {
-				dist[next_idx][next_bus] = {new_fee, new_time};
-				pq.push(make_tuple(new_fee, new_time, next_idx, next_bus, next_order));
-			}
-		}
-	}
-
-	pair<long long, int> ans = {INF, INF};
-	for (int i = 1; i <= N; i++) {
-		ans = min(ans, dist[B][i]);
-	}
-
-	if (ans.first == INF) cout << "-1 -1\n";
-	else cout << ans.first << " " << ans.second << "\n";
-	return 0;
+    cout << min_cost << " " << min_time;
+    return 0;
 }
