38d5851370b7d02f19857f7371c5df930295bbd8
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-11-26 23:48:28 +0900

    Title: 트리 정점 거리, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241126/트리 정점 거리/node-distance.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+using namespace std;
+
+#define MAX_N 1000
+
+int n, m;
+vector<pair<int, int>> edge[MAX_N + 1];
+bool visited[MAX_N + 1];
+
+int DFS(int start, int end, int dist) {
+	visited[start] = true;
+	for (int i = 0; i < (int)edge[start].size(); i++) {
+		int next_idx, next_dist;
+		tie(next_idx, next_dist) = edge[start][i];
+
+		if (visited[next_idx]) continue;
+
+		if (next_idx == end) {
+			return dist + next_dist;
+		}
+
+		int rslt = DFS(next_idx, end, dist + next_dist);
+		if (rslt != -1) return rslt;
+	}
+	return -1;
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i < n; i++) {
+		int v1, v2, d;
+		cin >> v1 >> v2 >> d;
+		edge[v1].push_back({ v2, d });
+		edge[v2].push_back({ v1, d });
+	}
+
+	for (int i = 1; i <= m; i++) {
+		int v1, v2;
+		cin >> v1 >> v2;
+		fill(visited, visited + n + 1, false);
+		cout << DFS(v1, v2, 0) << "\n";
+	}
+
+	return 0;
+}
