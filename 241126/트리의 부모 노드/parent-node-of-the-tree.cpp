59c81271d405f23bb67055ad9be0fea66e3f4a92
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-11-26 18:51:28 +0900

    Title: 트리의 부모 노드, Time: 57ms, Memory: 4MB, Status: Passed - CodeTree

--- 241126/트리의 부모 노드/parent-node-of-the-tree.cpp
+++ 241126/트리의 부모 노드/parent-node-of-the-tree.cpp
@@ -7,6 +7,17 @@ using namespace std;
 int n;
 vector<int> graph[MAX_N + 1];
 int parent[MAX_N + 1];
+bool visited[MAX_N + 1];
+
+void traversal(int root) {
+	for (int i = 0; i < (int)graph[root].size(); i++) {
+		int y = graph[root][i];
+		if (visited[y]) continue;
+		visited[y] = true;
+		parent[y] = root;
+		traversal(y);
+	}
+}
 
 int main() {
 	cin >> n;
@@ -15,9 +26,11 @@ int main() {
 		cin >> v1 >> v2;
 		graph[v1].push_back(v2);
 		graph[v2].push_back(v1);
-		parent[v2] = v1;
 	}
 
+	fill(visited, visited + MAX_N + 1, false);
+	visited[1] = true;
+	traversal(1);
 	for (int i = 2; i <= n; i++) {
 		cout << parent[i] << "\n";
 	}
