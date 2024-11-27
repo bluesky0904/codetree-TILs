c0b54bd415c8f3f31548b6fc014626c2a3db75a8
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-11-27 15:53:15 +0900

    Title: 트리 파악, Time: 1060ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

--- 241127/트리 파악/identifying-the-tree.cpp
+++ 241127/트리 파악/identifying-the-tree.cpp
@@ -1,12 +1,6 @@
-/*
-		1
-	*2		3
-			4
-			*5
-*/
-
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 #define MAX_N 100000
@@ -19,53 +13,51 @@ int parent[MAX_N + 1];
 int n;
 
 void DFS(int x) {
-	bool is_leaf = true;
-	for (int i = 0; i < (int)edge[x].size(); i++) {
-		int next_x = edge[x][i];
+    visited[x] = true;
+    bool is_leaf = true;
 
-		if (visited[next_x]) continue;
+    for (int next_x : edge[x]) {
+        if (!visited[next_x]) {
+            is_leaf = false;
+            parent[next_x] = x;
+            DFS(next_x);
+        }
+    }
 
-		visited[next_x] = true;
-		is_leaf = false;
-
-		DFS(next_x);
-	}
-	if (is_leaf) leaf.push_back(x);
+    if (is_leaf) {
+        leaf.push_back(x);
+    }
 }
 
 int main() {
-	cin >> n;
-	for (int i = 1; i < n; i++) {
-		int v1, v2;
-		cin >> v1 >> v2;
-		parent[v2] = v1;
-		edge[v1].push_back(v2);
-		edge[v2].push_back(v1);
-	}
+    cin >> n;
+    for (int i = 1; i < n; i++) {
+        int v1, v2;
+        cin >> v1 >> v2;
+        edge[v1].push_back(v2);
+        edge[v2].push_back(v1);
+    }
 
-	fill(visited, visited + MAX_N + 1, false);
-	visited[1] = true;
-	DFS(1);
+    fill(visited, visited + MAX_N + 1, false);
+    visited[1] = true;
+    DFS(1);
 
-	// leaf : 2 5 cnt = 0
-	// a : 5 -> 4, leaf : 2 4 cnt = 1
-	// b : 4 -> 3, leaf : 2 3 cnt = 2
-	// a : 3 -> 1, leaf : 2 cnt = 3
-	// b : 2 -> 1, leaf : x cnt = 4
-	// a 불가 즉 0이 답 cnt = 5
+    queue<int> q;
+    for (int l : leaf) q.push(l);
 
-	int cnt = 0;
-	while (!leaf.empty()) {
-		int horse = leaf.back();
-		leaf.pop_back(); 
+    int cnt = 0;
+    while (!q.empty()) {
+        int horse = q.front();
+        q.pop();
 
-		int next_horse = parent[horse];
-		if(next_horse != 1) leaf.push_back(next_horse);
+        int next_horse = parent[horse];
+        if (next_horse != 1) q.push(next_horse);
 
-		cnt++;
-	}
-	
-	if (cnt % 2 == 0) cout << 0 << "\n";
-	else cout << 1 << "\n";
-	return 0;
+        cnt++;
+    }
+
+    if (cnt % 2 == 0) cout << 0 << "\n";
+    else cout << 1 << "\n";
+
+    return 0;
 }
