892043d03b8955e8c1a18b3d06ecf0da779eb8fc
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-11-30 14:43:04 +0900

    Title: 최소 스패닝 트리, Time: 108ms, Memory: 1MB, Status: Passed - CodeTree

--- 241130/최소 스패닝 트리/minimum-spanning-tree.cpp
+++ 241130/최소 스패닝 트리/minimum-spanning-tree.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <tuple>
 #include <algorithm>
 using namespace std;
@@ -7,8 +6,8 @@ using namespace std;
 #define MAX_N 10000
 #define MAX_M 100000
 
-vector<tuple<int, int, int>> edge;
 int n, m;
+tuple<int, int, int> edge[MAX_M + 1];
 int uf[MAX_N + 1];
 
 int Find(int x) {
@@ -25,27 +24,26 @@ void Union(int x, int y) {
 
 int main() {
 	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
+	for (int i = 1; i <= m; i++) {
 		int v1, v2, d;
 		cin >> v1 >> v2 >> d;
-		edge.push_back(make_tuple(d, v1, v2));
+		edge[i] = make_tuple(d, v1, v2);
 	}
 
-	sort(edge.begin(), edge.end());
-
+	int total_weight = 0;
+	sort(edge + 1, edge + m + 1);
 	for (int i = 1; i <= n; i++)
 		uf[i] = i;
 
-	int ans = 0;
-	for (int i = 0; i < m; i++) {
+	for (int i = 1; i <= m; i++) {
 		int d, v1, v2;
 		tie(d, v1, v2) = edge[i];
 		if (Find(v1) != Find(v2)) {
-			ans += d;
 			Union(v1, v2);
+			total_weight += d;
 		}
 	}
 
-	cout << ans << "\n";
+	cout << total_weight << "\n";
 	return 0;
 }
