cff4dc10b13bbddf6927354352cac2dd1ac92c5c
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-12-06 17:17:46 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241206/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
+++ 241206/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
@@ -1,76 +1,31 @@
-/*
-사람 : n명, 원형으로 앉아 있음. 답이 될 수 있는 범위 중 항상 가운데 값을 고름.
-컴퓨터 : 1이상 m이하의 수 중 하나를 선택
-사람 A가 그 숫자를 예측
-컴퓨터는 사람이 예측한 숫자가 자신이 선택한 숫자보다 작은지 큰지 말함
-작거나 클 경우 다음 사람 턴으로 넘어감
-같을 경우 그 사람이 벌칙을 받음
-
-컴퓨터가 처음 a이상 b이하인 수 만을 선택한다 했을 때,
-게임이 가장 적게 지속될 때와 가장 오래 지속 될 때를 계산하는 프로그램
-
-예제 1
-입력 : 13 4 6
-출력 3 4
-
-컴퓨터 예측 : 4
-1과 13의 중앙값 7
-1과 6의 중앙값 3
-4와 6의 중앙값 5
-4와 4의 중앙값 4 ... 4회 ... 최대
-
-컴퓨터 예측 : 5
-1과 13의 중앙값 7
-1과 6의 중앙값 3
-4와 6의 중앙값 5 ... 3회 ... 최소
-
-컴퓨터 예측 : 6
-1과 13의 중앙값 7
-1과 6의 중앙값 3
-4와 6의 중앙값 5
-6과 6의 중앙값 6 ... 4회
-*/
-
 #include <iostream>
-#include <algorithm>
+#include <cmath> // for log2
 using namespace std;
 
-#define MAX_M 100000
-int m, a, b;
-int arr[MAX_M];
-
-int binary_search(int target) {
-	int idx = -1;
-	int cnt = 0;
-	int left = 0, right = m - 1;
-	while (left <= right) {
-		int mid = (left + right) / 2;
-		cnt++;
-		if (arr[mid] == target) {
-			return cnt;
-		}
-
-		if (arr[mid] > target)
-			right = mid - 1;
-		else
-			left = mid + 1;
-	}
-	return -1;
-}
-
 int main() {
-	cin >> m >> a >> b;
-	for (int i = 0; i < m; i++) {
-		arr[i] = i + 1;
-	}
-	int min_cnt = m, max_cnt = 0;
-	for (int target = a; target <= b; target++) {
-		int rslt = binary_search(target);
-		if (rslt == -1) continue;
-		min_cnt = min(min_cnt, rslt);
-		max_cnt = max(max_cnt, rslt);
-	}
+    int m, a, b;
+    cin >> m >> a >> b;
 
-	cout << min_cnt << " " << max_cnt << "\n";
-	return 0;
+    int min_cnt = 1e9, max_cnt = 0;
+
+    for (int target = a; target <= b; target++) {
+        int cnt = 0;
+        int left = 1, right = m;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            cnt++;
+            if (mid == target) break;
+            if (mid > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        min_cnt = min(min_cnt, cnt);
+        max_cnt = max(max_cnt, cnt);
+    }
+
+    cout << min_cnt << " " << max_cnt << "\n";
+    return 0;
 }
