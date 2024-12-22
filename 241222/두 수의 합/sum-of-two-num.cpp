00ca2bcafbd8d5a10104a6f246f0129c0258d117
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-12-22 16:31:53 +0900

    Title: 두 수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241222/두 수의 합/sum-of-two-num.cpp
+++ 241222/두 수의 합/sum-of-two-num.cpp
@@ -9,18 +9,23 @@ unordered_map<int, int> m;
 int arr[MAX_N + 1];
 
 int main() {
-	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		int num; cin >> num;
-		arr[i] = num;
-		m[num] = i;
-	}
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        int num;
+        cin >> num;
+        arr[i] = num;
+        m[num] = i;
+    }
 
-	int cnt = 0;
-	for (int i = 1; i <= n/2; i++) {
-		if (m.find(k - arr[i]) != m.end()) cnt++;
-	}
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        if (m.find(k - arr[i]) != m.end() && m[k - arr[i]] > i) {
+            cnt++;
+            m.erase(arr[i]);       // 중복 방지
+            m.erase(k - arr[i]);   // 쌍으로 이루어진 값 제거
+        }
+    }
 
-	cout << cnt << "\n";
-	return 0;
+    cout << cnt << "\n"; // cnt는 중복 없이 계산됨
+    return 0;
 }
