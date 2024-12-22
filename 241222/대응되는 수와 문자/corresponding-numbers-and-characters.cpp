a37aa1b51144a4f07de302f538740decbfa2d402
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-12-22 15:59:05 +0900

    Title: 대응되는 수와 문자, Time: 22ms, Memory: 1MB, Status: Runtime Error - CodeTree

--- 241222/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
+++ 241222/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -3,18 +3,11 @@
 #include <string>
 using namespace std;
 
-#define MAX_N 100000
+#define MAX_N 10000
 
 int n, m;
 unordered_map<string, int> string_to_num;
 string num_to_string[MAX_N + 1];
-
-bool is_number(string s) {
-	for (char c : s) {
-		if (!isdigit(c)) return false;
-	}
-	return true;
-}
 
 int main() {
 	cin >> n >> m;
@@ -24,11 +17,11 @@ int main() {
 		num_to_string[i] = s;
 	}
 
-	for (int i = 1; i <= m; i++) {
+	while (m--) {
 		string s; cin >> s;
-		if (is_number(s))cout << num_to_string[stoi(s)] << "\n";
-		else cout << string_to_num[s] << "\n";
+		if ('0' <= s[0] && s[0] <= '9')
+			cout << num_to_string[stoi(s)] << "\n";
+		else
+			cout << string_to_num[s] << "\n";
 	}
-
-	return 0;
 }
