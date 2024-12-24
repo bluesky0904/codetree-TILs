d2b184de81488e8f69eea152b4e968b21595171b
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2024-12-25 00:58:05 +0900

    Title: 코드트리 투어, Time: 1051ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 241225/코드트리 투어/codetree-tour.cpp
+++ 241225/코드트리 투어/codetree-tour.cpp
@@ -12,42 +12,50 @@ using namespace std;
 int q, n, m;
 int source;
 vector<pair<int, int>> graph[MAX_N];
-vector<int> dist(MAX_N, INF); // 벡터로 변경
-map<int, pair<int, int>> travel_list;
-map<int, vector<int>> dist_cache; // 출발지별 최단 거리 캐싱
+vector<int> dist(MAX_N, INF); // 최단 거리
+map<int, pair<int, int>> travel_list; // 상품 목록
+priority_queue<tuple<int, int, int>> pq; // (profit, -id, dest) 우선순위 큐
 
-// 다익스트라 알고리즘 (캐싱 활용)
-void set_source() {
-    if (dist_cache.find(source) != dist_cache.end()) {
-        dist = dist_cache[source]; // 캐싱된 결과 사용
-        return;
-    }
+// 다익스트라 알고리즘
+void dijkstra(int start) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> min_pq;
+    fill(dist.begin(), dist.end(), INF);
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-    fill(dist.begin(), dist.end(), INF); // 초기화
+    dist[start] = 0;
+    min_pq.push({0, start});
+    while (!min_pq.empty()) {
+        int cur_dist, cur_node;
+        tie(cur_dist, cur_node) = min_pq.top();
+        min_pq.pop();
 
-    dist[source] = 0;
-    pq.push({0, source});
-    while (!pq.empty()) {
-        int min_dist, min_idx;
-        tie(min_dist, min_idx) = pq.top();
-        pq.pop();
+        if (dist[cur_node] < cur_dist) continue;
 
-        if (dist[min_idx] != min_dist) continue;
+        for (const auto& edge : graph[cur_node]) {
+            int next_node = edge.first;
+            int weight = edge.second;
 
-        for (const auto& edge : graph[min_idx]) {
-            int next_idx = edge.first;
-            int next_dist = edge.second;
-
-            int new_dist = min_dist + next_dist;
-            if (dist[next_idx] > new_dist) {
-                dist[next_idx] = new_dist;
-                pq.push({new_dist, next_idx});
+            if (dist[next_node] > cur_dist + weight) {
+                dist[next_node] = cur_dist + weight;
+                min_pq.push({dist[next_node], next_node});
             }
         }
     }
+}
 
-    dist_cache[source] = dist; // 계산 결과 캐싱
+// 우선순위 큐 갱신
+void update_priority_queue() {
+    priority_queue<tuple<int, int, int>> new_pq;
+
+    for (const auto& [id, info] : travel_list) {
+        int revenue = info.first;
+        int dest = info.second;
+
+        if (dist[dest] == INF || revenue < dist[dest]) continue; // 판매 불가 상품
+        int profit = revenue - dist[dest];
+        new_pq.push({profit, -id, dest});
+    }
+
+    pq = move(new_pq); // 업데이트된 우선순위 큐로 교체
 }
 
 // 코드트리 랜드 건설
@@ -56,64 +64,59 @@ void construct_land() {
     for (int i = 0; i < m; i++) {
         int v1, v2, d;
         cin >> v1 >> v2 >> d;
-        if (v1 == v2) {
-            if (graph[v1].empty() || graph[v1][0].second > d) {
-                graph[v1].clear(); // 기존 간선 삭제
-                graph[v1].push_back({v1, d}); // 최소 가중치 간선 추가
-            }
-        } else {
-            graph[v1].push_back({v2, d});
-            graph[v2].push_back({v1, d});
-        }
+        graph[v1].push_back({v2, d});
+        graph[v2].push_back({v1, d});
     }
+    dijkstra(source);
+    update_priority_queue();
 }
 
 // 최적의 여행 상품 판매
 void sell_product() {
-    priority_queue<pair<int, int>> choose_list;
+    while (!pq.empty()) {
+        auto [profit, neg_id, dest] = pq.top();
+        int id = -neg_id;
+        pq.pop();
 
-    for (const auto& [id, info] : travel_list) {
-        int revenue = info.first;
-        int dest = info.second;
+        // 유효성 검사 (삭제된 상품 또는 업데이트된 dist 확인)
+        if (travel_list.find(id) == travel_list.end() || dist[dest] == INF) continue;
+        if (travel_list[id].first - dist[dest] != profit) continue;
 
-        if (dist[dest] == INF || revenue < dist[dest]) continue; // 판매 불가 조건
-
-        int profit = revenue - dist[dest];
-        choose_list.push({profit, -id}); // id를 음수로 저장해 오름차순 처리
-    }
-
-    if (choose_list.empty()) {
-        cout << -1 << "\n";
-    } else {
-        int id = -choose_list.top().second; // 가장 높은 우선순위 상품 선택
+        // 상품 판매
         cout << id << "\n";
-        travel_list.erase(id); // 관리 목록에서 제거
+        travel_list.erase(id);
+        return;
     }
+    cout << -1 << "\n";
 }
 
+// 메인 함수
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
 
     source = 0;
-
     cin >> q;
     while (q--) {
-        int command; cin >> command;
+        int command;
+        cin >> command;
         if (command == 100) {
             construct_land();
-            set_source();
         } else if (command == 200) {
             int id, revenue, dest;
             cin >> id >> revenue >> dest;
-            travel_list[id] = make_pair(revenue, dest);
+            travel_list[id] = {revenue, dest};
+            update_priority_queue();
         } else if (command == 300) {
-            int id; cin >> id;
+            int id;
+            cin >> id;
             travel_list.erase(id);
+            update_priority_queue();
         } else if (command == 400) {
             sell_product();
         } else if (command == 500) {
             cin >> source;
-            set_source();
+            dijkstra(source);
+            update_priority_queue();
         }
     }
 
