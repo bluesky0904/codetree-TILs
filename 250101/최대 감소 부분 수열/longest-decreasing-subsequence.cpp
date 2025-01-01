c17152d400b4b3bb44f02fac7288c258c935b686
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2025-01-01 17:45:21 +0900

    Title: 최대 감소 부분 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250101/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
+++ 250101/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
@@ -5,8 +5,8 @@ using namespace std;
 #define MAX_N 1000
 
 int n;
-int dp[MAX_N + 1];
-int a[MAX_N + 1];
+int a[MAX_N];
+int dp[MAX_N];
 
 void Initialize() {
 	for (int i = 0; i < n; i++)
@@ -14,20 +14,20 @@ void Initialize() {
 }
 
 int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
 	cin >> n;
 	for (int i = 0; i < n; i++)
 		cin >> a[i];
 
 	Initialize();
 
-	for (int i = 1; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < i; j++) {
-			if (a[j] > a[i])
-				dp[i] = max(dp[i], dp[j] + 1);
+			if (a[j] > a[i]) dp[i] = max(dp[i], dp[j] + 1);
 		}
 	}
 
-	int ans = 0;
+	int ans = INT_MIN;
 	for (int i = 0; i < n; i++) {
 		ans = max(ans, dp[i]);
 	}
