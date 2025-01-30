4d49f84ff183194e498ec3a13e460028cc8652ad
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2025-01-30 17:27:11 +0900

    Title: 미로 타워 디펜스, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250130/미로 타워 디펜스/maze-tower-defense.cpp
+++ 250130/미로 타워 디펜스/maze-tower-defense.cpp
@@ -1,31 +1,3 @@
-/*
-나선형 미로 : n x n, 1-based
-몬스터 : 1번, 2번, 3번
-플레이어 : 격자 속 가운데 탑에서 몬스터를 제거하려고 함
-점수
-
-1. 플레이어는 상하좌우 방향 중 주어진 공격 칸 수만큼 몬스터를 공격하여 없앨 수 있음
-공격방향 / 공격칸수
-
-2. 비어있는 공간만큼 몬스터는 앞으로 이동하여 빈 공간을 채움
-
-3. 이때 몬스터의 종류가 4번 이상 반복하여 나오면 해당 몬스터 또한 삭제
-해당 몬스터들은 동시에 사라짐
-
-삭제된 이후에는 몬스터들을 앞으로 당겨주고, 4번 이상 나오는 몬스터가 있을 경우 또 삭제를 해줌. 
-4번 이상 나오는 몬스터가 없을 때까지 반복
-
-4. 삭제가 끝난 다음에는 몬스터를 차례대로 나열했을 때 같은 숫자끼리 짝을 지어줌.
-이후 각각의 짝을 (총 개수, 숫자의 크기)로 바꾸어서 다시 미로 속에 집어넣음
-
-만약 새로 생긴 배열이 원래 격자의 범위를 넘는다면 나머지 배열은 무시
-
-해당 과정이 끝나면 한 라운드가 끝
-1과 3 과정에서 삭제되는 몬스터의 번호는 점수에 합쳐짐
-(삭제되는 몬스터의 번호 x 삭제되는 몬스터의 숫자)의 합
-
-각 라운드별로 공격 방향과 공격 칸수가 주어질 때 모든 라운드가 끝난 후 플레이어가 얻게되는 점수?
-*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
@@ -133,6 +105,7 @@ void remove() {
 
 	while (1) {
 		int size = (int)monsters.size();
+		if (size == 0) break; // 예외 방지
 		bool is_over = true;
 		int cnt = 1;
 
@@ -141,11 +114,11 @@ void remove() {
 			if (monsters[i] == monsters[i + 1]) {
 				if(i != size - 2) cnt++;
 				else {
-					cnt++;
+					cnt++; // 이 부분 빼먹어서 틀렸음
 					if (cnt >= 4) {
 						tmp[monsters[i]] += cnt;
 						for (int j = 0; j < cnt; j++) {
-							monsters[i + 1- j] = 0;
+							monsters[i + 1 - j] = 0;
 						}
 						is_over = false;
 						cnt = 1;
@@ -159,25 +132,23 @@ void remove() {
 						monsters[i - j] = 0;
 					}
 					is_over = false;
-					cnt = 1;
 				}
-				else cnt = 1;
+				cnt = 1;
 			}
 		}
 
 		if (is_over) break;
-		else {
-			next_monsters.clear();
+		next_monsters.clear();
 
-			for (int i = 0; i < (int)monsters.size(); i++) {
-				if (monsters[i] > 0) next_monsters.push_back(monsters[i]);
-			}
-
-			monsters.clear();
-			for (int i = 0; i < (int)next_monsters.size(); i++) {
-				monsters.push_back(next_monsters[i]);
-			}
+		for (int i = 0; i < (int)monsters.size(); i++) {
+			if (monsters[i] > 0) next_monsters.push_back(monsters[i]);
 		}
+
+		monsters.clear();
+		for (int i = 0; i < (int)next_monsters.size(); i++) {
+			monsters.push_back(next_monsters[i]);
+		}
+		
 	}
 
 	for (int i = 1; i <= MONSTER_TYPE; i++) {
@@ -197,21 +168,29 @@ void rearrange() {
 			else {
 				cnt++;
 				next_monsters.push_back(cnt);
+				if ((int)next_monsters.size() == (n * n - 1)) break;
 				next_monsters.push_back(monsters[i]);
+				if ((int)next_monsters.size() == (n * n - 1)) break;
 			}
 		}
 		else {
 			if (i != size - 2) {
 				next_monsters.push_back(cnt);
+				if ((int)next_monsters.size() == (n * n - 1)) break;
 				next_monsters.push_back(monsters[i]);
+				if ((int)next_monsters.size() == (n * n - 1)) break;
 				cnt = 1;
 			}
 			else {
 				next_monsters.push_back(cnt);
+				if ((int)next_monsters.size() == (n * n - 1)) break;
 				next_monsters.push_back(monsters[i]);
+				if ((int)next_monsters.size() == (n * n - 1)) break;
 
 				next_monsters.push_back(1);
+				if ((int)next_monsters.size() == (n * n - 1)) break;
 				next_monsters.push_back(monsters[i + 1]);
+				if ((int)next_monsters.size() == (n * n - 1)) break;
 			}
 		}
 	}
@@ -268,8 +247,6 @@ int main() {
 	}
 
 	setCounterClockDir();
-	//cout << "COUNTER CLOCK DIR" << "\n";
-	
 
 	for (int i = 1; i <= m; i++) {
 		//cout << "TURN : " << i << "\n";
