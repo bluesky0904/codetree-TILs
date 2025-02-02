abcc9f0d56914287a1707e678ddb397e230954b4
bluesky0904 <132040048+bluesky0904@users.noreply.github.com>
2025-02-02 15:34:59 +0900

    Title: 미로 타워 디펜스, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250202/미로 타워 디펜스/maze-tower-defense.cpp
+++ 250202/미로 타워 디펜스/maze-tower-defense.cpp
@@ -1,251 +1,250 @@
+/*
+나선형 미로 : n x n int grid[][]
+몬스터 : 1번, 2번, 3번 vector<int> monsters, vector<int> next_monsters
+플레이어 : 격자의 중심에 위치
+점수 score
+
+1. 플레이어는 상하좌우 방향 중 주어진 공격 칸 수만큼 몬스터를 공격하여 없앨 수 있음
+
+2. 비어있는 공간만큼 몬스터는 앞으로 이동하여 빈 공간을 채움
+
+3. 이때 몬스터의 종류가 4번 이상 반복하여 나오면 해당 몬스터 또한 삭제됨
+해당 몬스터들은 동시에 사라짐
+삭제된 이후에는 몬스터들을 앞으로 당겨주고, 4번 이상 나오는 몬스터가 있을 경우 또 삭제를 해줌
+4번 이상 나오는 몬스터가 없을 때까지 반복
+
+
+4. 삭제가 끝난 다음에는 몬스터를 차례대로 나열했을 때 같은 숫자끼리 짝을 지어줌
+이후 각각의 짝을 (총 개수, 숫자의 개수)로 바꾸어서 다시 미로 속에 집어넣음
+만약 새로 생긴 배열이 원래 격자의 범위를 넘는다면 나머지 배열은 무시
+
+해당 과정이 끝나면 한 라운드가 끝남
+1과 3 과정에서 삭제되는 몬스터의 번호는 점수에 합쳐짐
+score += (몬스터의 번호 x 삭제된 수)
+
+각 라운드별로 공격 방향과 공격하는 칸 수가 주어질 때 모든 라운드가 끝난 후 플레이어가 얻게되는 점수는 몇 점인지 출력
+*/
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-#include <tuple>
+using namespace std;
 
 #define MAX_N 25
 #define DIR_NUM 4
-
-using namespace std;
+#define MONSTER_NUM 3
 
 int n, m;
+int grid[MAX_N + 1][MAX_N + 1];
+vector<int> monsters;
+vector<int> next_monsters;
+int score;
+int counter_clock_dir[MAX_N + 1][MAX_N + 1];
 
-int grid[MAX_N][MAX_N];
-int temp[MAX_N][MAX_N];
+int dx[DIR_NUM] = {0,1,0,-1};
+int dy[DIR_NUM] = {1,0,-1,0};
 
-vector<pair<int, int> > spiral_points;
-
-int ans;
-
-void SearchSpiral() {
-    // 나선이 돌아가는 순서대로 
-    // 왼쪽 아래 오른쪽 위 방향이 되도록 정의합니다.
-    int dx[DIR_NUM] = {0, 1, 0, -1};
-    int dy[DIR_NUM] = {-1, 0, 1, 0};
-
-    // 시작 위치와 방향, 
-    // 해당 방향으로 이동할 횟수를 설정합니다. 
-    int curr_x = n / 2, curr_y = n / 2;
-    int move_dir = 0, move_num = 1;
-
-    while(curr_x || curr_y) {
-        // move_num 만큼 이동합니다.
-        for(int i = 0; i < move_num; i++) {
-            curr_x += dx[move_dir]; curr_y += dy[move_dir];
-            spiral_points.push_back(make_pair(curr_x, curr_y));
-
-            // 이동하는 도중 (0, 0)으로 오게 되면,
-            // 움직이는 것을 종료합니다.
-            if(!curr_x && !curr_y)
-                break;
-        }
-        
-        // 방향을 바꿉니다.
-        move_dir = (move_dir + 1) % 4;
-        // 만약 현재 방향이 왼쪽 혹은 오른쪽이 된 경우에는
-        // 특정 방향으로 움직여야 할 횟수를 1 증가시킵니다.
-        if(move_dir == 0 || move_dir == 2)
-            move_num++;
-    }
+void setCounterClock() {
+	int cx = (n + 1) / 2, cy = ((n + 1) / 2), cd = 2;
+	int stride = 1;
+	while (!(cx == 1 && cy == 1)) {
+		for (int cnt = 1; cnt <= 2; cnt++) {
+			for (int dist = 1; dist <= stride; dist++) {
+				counter_clock_dir[cx][cy] = cd;
+				cx += dx[cd], cy += dy[cd];
+				if (cx == 1 && cy == 1) return;
+			}
+			cd = (cd + 3) % DIR_NUM;
+		}
+		stride++;
+	}
 }
 
-void Pull() {
-    // Step 1. temp 배열을 초기화합니다.
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            temp[i][j] = 0;
-
-    // Step2. 나선 순서대로 보며
-    //        비어있지 않은 값들을 temp에 채워줍니다.
-    int temp_idx = 0;
-    for(pair<int, int> grid_point: spiral_points) {
-        int gx, gy;
-        tie(gx, gy) = grid_point;
-        
-        if(grid[gx][gy]) {
-            int tx, ty;
-            tie(tx, ty) = spiral_points[temp_idx];
-            temp[tx][ty] = grid[gx][gy];
-            temp_idx++;
-        }
-    }
-
-    // Step 3. temp 값을 다시 grid에 옮겨줍니다.
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            grid[i][j] = temp[i][j];
+bool inRange(int x, int y) {
+	return (1 <= x && x <= n && 1 <= y && y <= n);
 }
 
-void Attack(int d, int p) {
-    // 문제에서 주어진 순서대로 → ↓ ← ↑
-    int dx[DIR_NUM] = {0, 1,  0, -1};
-    int dy[DIR_NUM] = {1, 0, -1,  0};
+void attackMonster(int d, int p) {
+	int tmp[MONSTER_NUM + 1] = { 0, };
 
-    // Step 1. d 방향으로 p마리의 몬스터를 제거합니다.
-    int center_x = n / 2, center_y = n / 2;
-    for(int dist = 1; dist <= p; dist++) {
-        int nx = center_x + dx[d] * dist;
-        int ny = center_y + dy[d] * dist;
+	int cx = (n + 1) / 2, cy = (n + 1) / 2, cd = d;
+	for (int dist = 1; dist <= p; dist++) {
+		int nx = cx + dx[cd] * dist, ny = cy + dy[cd] * dist;
+		if (inRange(nx, ny) && grid[nx][ny] > 0) {
+			tmp[grid[nx][ny]]++;
+			grid[nx][ny] = 0;
+		}
+	}
 
-        ans += grid[nx][ny];
-        grid[nx][ny] = 0; 
-    }
-
-    // Step2. 비어 있는 자리를 당겨서 채워줍니다.
-    Pull();
+	for (int i = 1; i <= MONSTER_NUM; i++) {
+		score += (i * tmp[i]);
+	}
 }
 
-int GetNumBySpiralIdx(int spiral_idx) {
-    int x, y;
-    tie(x, y) = spiral_points[spiral_idx];
-    return grid[x][y];
+void gridToVec() {
+	monsters.clear();
+
+	int cx = (n + 1) / 2, cy = ((n + 1) / 2) - 1, cd = counter_clock_dir[cx][cy];
+	while (!(cx == 1 && cy == 1)) {
+		if(grid[cx][cy] > 0) monsters.push_back(grid[cx][cy]);
+		cd = counter_clock_dir[cx][cy];
+		cx += dx[cd], cy += dy[cd];
+
+		if (cx == 1 && cy == 1) break;
+	}
 }
 
-// start_idx로부터 연속하여 같은 숫자로 이루어져 있는
-// 가장 끝 index를 찾아 반환합니다. 
-int GetEndIdxOfSameNum(int start_idx) {
-    int end_idx = start_idx + 1;
-    int curr_num = GetNumBySpiralIdx(start_idx);
-    int end_of_array = (int) spiral_points.size();
+void remove() {
+	int tmp[MONSTER_NUM + 1] = { 0, };
+	
+	while(1){
+		int cnt = 1;
+		int size = (int)monsters.size();
+		bool is_over = true;
 
-    while(end_idx < end_of_array) {
-        if(GetNumBySpiralIdx(end_idx) == curr_num)
-            end_idx++;
-        else
-            break;
-    }
+		for (int i = 0; i < size - 1; i++) {
+			if (monsters[i] == monsters[i + 1]) {
+				cnt++;
+				if (i == size - 2 && cnt >= 4) {
+					is_over = false;
+					tmp[monsters[i]] += cnt;
+					for (int j = 0; j < cnt; j++) {
+						monsters[i + 1 - j] = 0;
+					}
+				}
+			}
+			else {
+				if (cnt >= 4) {
+					is_over = false;
+					tmp[monsters[i]] += cnt;
+					for (int j = 0; j < cnt; j++) {
+						monsters[i - j] = 0;
+					}
+				}
+				cnt = 1;
+			}
+		}
 
-    return end_idx - 1;
+		if (is_over) break;
+		next_monsters.clear();
+		for (int i = 0; i < (int)monsters.size(); i++) {
+			if(monsters[i] > 0) next_monsters.push_back(monsters[i]);
+		}
+		monsters.clear();
+		for (int i = 0; i < (int)next_monsters.size(); i++) {
+			monsters.push_back(next_monsters[i]);
+		}
+	}
+
+	for (int i = 1; i <= MONSTER_NUM; i++) {
+		score += (i * tmp[i]);
+	}
 }
 
-void Remove(int start_idx, int end_idx) {
-    for(int i = start_idx; i <= end_idx; i++) {
-        int x, y;
-        tie(x, y) = spiral_points[i];
-        ans += grid[x][y];
-        grid[x][y] = 0;
-    }
+void rearrange() {
+	next_monsters.clear();
+
+	int cnt = 1;
+	int size = (int)monsters.size();
+	for (int i = 0; i < size - 1; i++) {
+		if (monsters[i] == monsters[i + 1]) {
+			cnt++;
+			if (i == size - 2) {
+				next_monsters.push_back(cnt);
+				if ((int)next_monsters.size() == ((n * n) - 1)) break;
+				next_monsters.push_back(monsters[i]);
+				if ((int)next_monsters.size() == ((n * n) - 1)) break;
+			}
+		}
+		else {
+			next_monsters.push_back(cnt);
+			if ((int)next_monsters.size() == ((n * n) - 1)) break;
+			next_monsters.push_back(monsters[i]);
+			if ((int)next_monsters.size() == ((n * n) - 1)) break;
+
+			if (i == size - 2) {
+				next_monsters.push_back(1);
+				if ((int)next_monsters.size() == ((n * n) - 1)) break;
+				next_monsters.push_back(monsters[i + 1]);
+				if ((int)next_monsters.size() == ((n * n) - 1)) break;
+			}
+			cnt = 1;
+		}
+	}
+
+	monsters.clear();
+	for (int i = 0; i < (int)next_monsters.size(); i++) {
+		monsters.push_back(next_monsters[i]);
+	}
 }
 
-// 4번 이상 반복하여 나오는 구간을 지워줍니다.
-bool Bomb() {
-    bool did_explode = false;
-    int curr_idx = 0;
-    int end_of_array = (int) spiral_points.size();
+void vecToGrid() {
+	for (int x = 1; x <= n; x++) {
+		for (int y = 1; y <= n; y++) {
+			grid[x][y] = 0;
+		}
+	}
 
-    while(curr_idx < end_of_array) {
-        int end_idx = GetEndIdxOfSameNum(curr_idx);
-        int curr_num = GetNumBySpiralIdx(curr_idx);
+	int cx = (n + 1) / 2, cy = ((n + 1) / 2) - 1, cd = counter_clock_dir[cx][cy];
+	for(int i = 0; i < (int)monsters.size(); i++) {
+		grid[cx][cy] = monsters[i];
+		cd = counter_clock_dir[cx][cy];
+		cx += dx[cd], cy += dy[cd];
 
-        // 맨 끝에 도달하게 되면, 더이상 진행하지 않습니다.
-        if(curr_num == 0)
-            break;
-
-        if(end_idx - curr_idx + 1 >= 4) {
-            // 연속한 숫자의 개수가 4개 이상이면
-            // 해당 구간을 지워줍니다.
-            Remove(curr_idx, end_idx);
-            did_explode = true;
-        }
-
-        // 그 다음 구간의 시작값으로 변경해줍니다.
-        curr_idx = end_idx + 1;
-    }
-
-    return did_explode;
+		if (cx == 1 && cy == 1) {
+			if(i + 1 < monsters.size() )grid[cx][cy] = monsters[i + 1];
+			break;
+		}
+	}
 }
 
-// 4번 이상 반복하여 나오는 구간을 계속 지워줍니다.
-void Organize() {
-    while(true) {
-        // 4번 이상 나오는 구간을 터뜨려봅니다.
-        bool keep_going = Bomb();
-        
-        if(!keep_going)
-            break;
-
-        // 지운 이후에는 다시 당겨서 채워줍니다.
-        Pull();
-    }
-}
-
-void LookAndSay() {
-    // Step 1. temp 배열을 초기화합니다.
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            temp[i][j] = 0;
-    
-    // Step2. 보고 말하며 temp에 해당 값을 기록합니다.
-    int temp_idx = 0;
-
-    int curr_idx = 0;
-    int end_of_array = (int) spiral_points.size();
-    while(curr_idx < end_of_array) {
-        int end_idx = GetEndIdxOfSameNum(curr_idx);
-
-        // 연속하여 나온 숫자의 개수와 숫자 종류 값을 계산합니다.
-        int contiguous_cnt = end_idx - curr_idx + 1;
-        int curr_num = GetNumBySpiralIdx(curr_idx);
-
-        // 맨 끝에 도달하게 되면, 더이상 진행하지 않습니다.
-        if(curr_num == 0)
-            break;
-
-        // temp에 (개수, 종류) 순서대로 기록해줍니다.
-        // 만약 격자를 벗어나면 종료합니다.
-        if(temp_idx >= end_of_array)
-            break;
-        
-        int tx, ty;
-        tie(tx, ty) = spiral_points[temp_idx];
-        temp[tx][ty] = contiguous_cnt;
-        temp_idx++;
-
-        if(temp_idx >= end_of_array)
-            break;
-
-        tie(tx, ty) = spiral_points[temp_idx];
-        temp[tx][ty] = curr_num;
-        temp_idx++;
-
-        // 그 다음 구간의 시작값으로 변경해줍니다.
-        curr_idx = end_idx + 1;
-    }
-
-    // Step 3. temp 값을 다시 grid에 옮겨줍니다.
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            grid[i][j] = temp[i][j];
-}
-
-void Simulate(int d, int p) {
-    // Step 1. 공격하여 몬스터를 제거합니다.
-    Attack(d, p);
-
-    // Step 2. 4번 이상 반복하여 나오는 구간을 계속 지워줍니다.
-    Organize();
-
-    // Step 3. 보고 말하기 행동을 진행합니다.
-    LookAndSay();
+void print() {
+	cout << "GRID" << "\n";
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cout << grid[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "MONSTERS" << "\n";
+	for (int i = 0; i < (int)monsters.size(); i++) {
+		cout << monsters[i] << " ";
+	}
+	cout << "\n";
+	cout << "SCORE : " << score << "\n";
 }
 
 int main() {
-    cin >> n >> m;
+	ios::sync_with_stdio(0); cin.tie(0);
+	freopen("input.txt", "r", stdin);
 
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            cin >> grid[i][j];
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> grid[i][j];
+		}
+	}
 
-    // 중심 탑을 기준으로 나선 모양으로 회전했을 때
-    // 지나게 되는 위치의 좌표들을 순서대로 기록해 놓습니다.
-    SearchSpiral();
+	score = 0;
+	setCounterClock();
 
-    // m번에 걸쳐 시뮬레이션을 진행합니다.
-    while(m--) {
-        int d, p;
-        cin >> d >> p;
+	for (int i = 1; i <= m; i++) {
+		int d, p; cin >> d >> p;
 
-        Simulate(d, p);
-    }
+		attackMonster(d, p);
+		//print();
 
-    cout << ans;
+		gridToVec();
+		//print();
+
+		remove();
+		//print();
+
+		rearrange();
+		//print();
+
+		vecToGrid();
+		//print();
+	}
+    cout << score << "\n";
+    return 0;
 }
